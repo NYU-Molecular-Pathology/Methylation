8b32e9a3c90ebf6b568a6c6582a020f6729867ef
jonserr <jnsrro@gmail.com>
2021-06-10 14:21:21 -0400

    Add files via upload

---
+++ LoadInstall_new.R
@@ -0,0 +1,274 @@
+# Some useful keyboard shortcuts for package authoring:
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+#devtools::install_github("RGLab/Rtsne.multicore")}
+#rstudioapi::executeCommand("toggleDocumentOutline")
+
+# Package Automation with container in Kubernetes: https://solutions.rstudio.com/sys-admin/launcher/kubernetes/
+
+sw <- function(pkgOb){try(return(suppressMessages(suppressWarnings(pkgOb))),silent=T)}
+rq <- function(pkg){return(sw(!require(pkg, character.only=T)))}
+ld <- function(libName){sw(suppressPackageStartupMessages(library(libName,character.only=T)))}
+up <- function(){update.packages(repos='http://cran.rstudio.com/', ask=F, checkBuilt=T)}
+
+# FUN: Installs package from Cran
+doInst <- function(func, param, ...){do.call(func,c(param,list(...)))}
+
+pk.inst <- function(pkg,...){
+  pk.opt <- list(pkg,dependencies=T,verbose=T,...)
+  message("Checking ", pkg, "...")
+  tryCatch(
+    expr={if(rq(pkg)){
+      do.call(install.packages, c(pk.opt,list(type="both")))
+    }else{message(pkg," ...load successful")}
+    },
+    warning=function(cond) {message("Warning caught:", cond, "\n-----------");up()
+      do.call(install.packages, c(pk.opt,list(type="both")))
+    },
+    custom_error=function(cond) {message("Error1 caught: ", cond, "\n-----------");up()
+      do.call(install.packages, c(pk.opt,list(type="source")))
+    },
+    error=function(cond) {message("Error2 caught: ", cond, "\n-----------")
+      do.call(install.packages, c(pk.opt,list(type="binary")))
+    },
+    finally={suppressMessages(do.call(easypackages::packages, list(pkg)))}
+  )
+}
+# FUN: Installs package from github link
+gh.inst <- function(pkNam, ghLnk,...){
+  message("Checking ", pkNam, "...")
+  gh.opt=list(ghLnk, dependencies=T, verbose=T, upgrade="always",...)
+  tryCatch(
+    expr={if(rq(pkNam)){
+      params <- c(gh.opt,list(type="both"))
+      do.call(devtools::install_github,params)
+    }else{message(pkNam," ...load successful")}
+    },
+    error=function(cond){
+      up()
+      params  <- c(gh.opt,list(type="binary"))
+      do.call(devtools::install_github, params)
+    }
+  )
+}
+
+# FUN: Installs package from Source link
+srcInst <- function(pkg,...){
+  message("Checking ", pkg, "...")
+  downLoc = file.path("~/Desktop",basename(pkg))
+  message("Downloading to:\n",downLoc)
+  download.file(url=pkg,destfile=downLoc)
+  tryCatch(
+    expr={
+      install.packages(downLoc, repos=NULL,dependencies=T,verbose=T, type="source")
+      },
+    error=function(cond){
+      install.packages(pkg,repos=NULL, method="libcurl")
+    },
+    warning=function(cond){
+      install.packages(pkg,repos=NULL, method="source")
+    },
+    finally={message(pkg," ...load successful")}
+  )
+}
+
+# FUN: Installs package from Bioconductor
+bioInst <- function(pknm){
+  message("Checking ", pknm, "...")
+  bio.opt <- list(pkgs=pknm, update=T, ask=F)
+  tryCatch(
+    expr={if(rq(pknm)){sw(do.call(BiocManager::install, c(bio.opt)))
+    }else{message(pknm," ...load successful")}},
+    error=function(cond){message("Package already loaded")}
+  )
+}
+
+fixProf <- function(){
+  txt1 <- "^[:blank:]*autoload\\(\"needs\", \"needs\"\\)"
+  txt2 <- "\n\nautoload(\"needs\", \"needs\")\n\n"
+  siteProf <- if(is.na(Sys.getenv("R_PROFILE", unset=NA))) {
+    file.path(Sys.getenv("R_HOME"),"etc","Rprofile.site")
+  } else {Sys.getenv("R_PROFILE")}
+  if (!file.exists(siteProf)) {file.create(siteProf)}
+  cxn <- file(siteProf); lines <- readLines(cxn)
+  if (!any(grepl(txt1,lines))) {write(txt2, file=siteProf, append=T)}
+  close(cxn)
+}
+
+fixNeeds <- function (){
+  sysfile <- system.file("extdata", "promptUser", package="needs")
+  write(0, file=sysfile); options(needs.promptUser=FALSE); needs:::autoload(TRUE)
+}
+
+checkNeeds <- function(){
+  tryCatch(
+    expr={
+      if(!("needs" %in% rownames(installed.packages()))){
+        install.packages("needs",dependencies=T,verbose=T)
+        fixNeeds();fixProf()
+      }else{
+        fixNeeds();fixProf()
+      }
+    },
+    error=function(cond){
+      devtools::install_github("joshkatz/needs", ref = "development",dependencies=T,verbose=T)
+      fixNeeds();fixProf()
+    },
+    warning=function(cond){
+      install.packages("needs",dependencies=T,verbose=T)
+      fixNeeds();fixProf()
+    },
+    finally={
+      message("needs ...install ", rq(needs))
+    }
+  )
+}
+
+setOptions <- function(){
+  options(needs.promptUser=F); Sys.setenv(R_ENABLE_JIT=T)
+  options("needs.promptUser"=F); options("promptUser"=F)
+  options("device.ask.default" ='1')
+  options("install.packages.compile.from.source"="Yes")
+  options("install.packages.check.source"="yes")
+  compiler::enableJIT(3); compiler::compilePKGS(enable=T);
+  compiler::setCompilerOptions(suppressAll=T, optimize=3)
+}
+
+cpMakeV <- function(editFile=F){
+  mkFi <- "/Volumes/CBioinformatics/jonathan/Rprojects/Methylation-scripts/Makevars"
+  message("Copying Makevars to local dotfiles: ", mkFi)
+  if(editFile){file.edit(mkFi)}
+  if(file.exists(mkFi)){file.copy(mkFi,"~/.R/", overwrite=T )}
+}
+
+# FUN: Load Classifier Versions -
+install.or.load <- function(pathtoFile=NULL, instNew=T, rmpkg=F) {
+  cbio="/Volumes/CBioinformatics"
+  if(is.null(pathtoFile)){
+    mnp.pk.loc = file.path(cbio, "Methylation/in_house/mnp.v116/mnp.v11b6")
+  }else{mnp.pk.loc<-pathtoFile}
+  if(rmpkg){remove.packages("mnp.v11b6")}
+  if(instNew){
+    install.packages(mnp.pk.loc, repos=NULL, type="source", force=T)
+    install.or.load(instNew=F)
+  }else{ld("mnp.v11b6")}
+}
+
+# Loads default packages or custom if input provided
+#' @param ezLibs  default is NULL, list of libraries to load or install
+#' @param ghPk dataframe of packages following the convention: packageName="gitRepo/directory"
+#' @return package installs or loads
+#' @export
+loadPacks <- function(ezLibs=NULL, ghPk=NULL, bioPks=NULL) {
+  pkgs <- c('devtools', 'remotes', 'sjmisc', 'stringi', 'digest', 'RCurl', 'rlang', 'parallel', 'grid', 'gridExtra','knitr', 'kableExtra','ggplot2', 'plotly', 'ggfortify', 'ggrepel', 'gplots', 'fastmatch','pals', 'Polychrome','qdapTools','beepr','xtable','pander','grDevices','graphics','stats', 'utils')
+  if(is.null(ezLibs)){
+    ezLibs<-c('tidyverse','sjmisc','stringi','digest','RCurl','gridExtra','needs')}
+  if(is.null(ghPk)){
+    ghPk<- data.frame(
+      docstring='dasonk/docstring',
+      rstudioapi='rstudio/rstudioapi',
+      easypackages='jakesherman/easypackages',
+      redcapAPI='nutterb/redcapAPI',
+      crayon="r-lib/crayon")
+  }
+
+  if(is.null(bioPks)){
+    bioPks <- c('HDF5Array','rngtools','bumphunter','minfi','lumi', 'ade4', 'methylumi',
+                'randomForest', 'glmnet',
+                'IlluminaHumanMethylation450kmanifest',
+                'IlluminaHumanMethylation450kanno.ilmn12.hg19',
+                'Rtsne', 'IlluminaHumanMethylationEPICmanifest',
+                'IlluminaHumanMethylationEPICanno.ilm10b4.hg19', 'MethylAid', 'conumee',
+                'BiocParallel')
+  }
+
+  tryCatch(
+    expr={setOptions(); fixProf()
+      if (rq("devtools")) {install.packages("devtools",dependencies=T,verbose=T)}else{library("devtools")}
+      checkNeeds()
+      gh.inst("easypackages","jakesherman/easypackages")
+      if (rq("tidyverse")) {pk.inst("tidyverse")}else{ld("tidyverse")}
+      easypackages::packages("parallel","doSNOW","doParallel", "foreach","compiler", prompt=F)
+      if(!require("BiocManager")){BiocManager::install(version="3.10",update=T, ask=F, type="source")}
+      #if(BiocManager::version()!='3.13'){BiocManager::install(version="3.13",update=T, ask=F, type="source")}
+      if(rq("zip")){install.packages("zip", dependencies=T, type="binary")}
+      invisible(lapply(1:length(pkgs),function(x){pk.inst(pkgs[x])}))
+      invisible(lapply(names(ghPk),function(x){gh.inst(x,ghPk[1,x])}))
+      invisible(lapply(1:length(bioPks),function(x){bioInst(bioPks[x])}))
+      # Loads libraries
+      ready <- ezLibs %in% names(sessionInfo()[["otherPkgs"]])
+      if(any(!ready)){lapply(ezLibs[!ready], require, character.only=TRUE)}
+    },
+    error= function(cond){
+      message("You encountered the following error during install:\n", cond,"\n")
+      message("If there is a compile error, try running fixCompiles() and then try loadPacks() again")
+    }
+  )
+
+}
+
+fixCompiles <- function(){
+  #system("/bin/bash -c $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)")
+  cpMakeV()
+  system("brew install llvm gcc")
+  system("defaults write org.R-project.R NSAppSleepDisabled -bool YES")
+  system('echo "export PATH="/usr/local/sbin:$PATH"" >> ~/.zshrc >')
+  system('echo "export PATH="/usr/local/sbin:$PATH"" >> ~/.bashrc >')
+  system("export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES")
+  system('echo "export OBJC_DISABLE_INITIALIZE_FORK_SAFETY="YES"" >> ~/.zshrc')
+  system('echo "export OBJC_DISABLE_INITIALIZE_FORK_SAFETY="YES"" >> ~/.bashrc')
+  system("xcode-select --install")
+  system("xcodebuild -runFirstLaunch")
+  system("brew install dpkg openssl libffi go python")
+  system('export PATH="/usr/local/opt/python/libexec/bin:$PATH"')
+  system('export GOROOT=/usr/local/opt/go/libexec')
+  system("export PATH=$PATH:$GOROOT/bin")
+  system("brew install cmake gsl gmp libopenmpt open-mpi")
+  # export RSTUDIO_WHICH_R=/usr/local/bin/R
+  system("brew install aspell gdal autoconf automake bash cairo cmake coreutils dos2unix exa fd fontconfig freetype fribidi fzf gawk gcc gdbm gdk-pixbuf gettext giflib git glib gmp gnu-sed gnu-tar gnutls graphite2 harfbuzz htop icu4c ilmbase imagemagick isl jpeg libcroco libde265 libevent libffi libgcrypt libgpg-error libheif libidn2 libmpc libomp libpng librsvg libtasn1 libtermkey libtiff libtool libunistring libuv libvterm libyaml little-cms2 llvm lua lua@5.1 luajit luarocks lzo mpfr msgpack multimarkdown ncurses neovim nettle nspr nss oniguruma openconnect openexr openjpeg openssl@1.1 p11-kit pandoc pandoc-citeproc pandoc-crossref pango pcre pcre2 pdfgrep perl pixman pkg-config poppler qt readline rename ripgrep ruby shared-mime-info shellcheck sqlite stoken swig texinfo tmux tree unbound unibilium vim webp wget x265")
+  system('export CFLAGS+=-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk
+  export CCFLAGS+=-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk
+  export CXXFLAGS+=-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk
+  export CPPFLAGS+=-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk')
+  system("brew install golang libstfl")
+  if(dir.exists("/Library/Developer/CommandLineTools")){
+    if(length(list.dirs("/Library/Developer/CommandLineTools", recursive = F))>0){
+      system("ln -s /Applications/Xcode.app/Contents/Developer /Library/Developer/CommandLineTools")}}
+  system("brew unlink gcc && brew link gcc --force")
+  system('brew link --overwrite gcc')
+  system('brew doctor')
+}
+
+#' Loads or installs required packages based if run is clinical or research
+#' @param isMC boolean logical if True, run is methylation clinical run set as false to load only research packages and not the methylation classifier
+#' @return
+#' @export
+setRunType <- function(isMC=T){
+
+  sexEst="https://github.com/jungch/sest/raw/master/sest.tar"
+  mgmtLn="https://github.com/badozor/mgmtstp27/raw/master/trunk/Rpackage/mgmtstp27_0.6-2.tar.gz"
+  if (isMC){
+    cbio="/Volumes/CBioinformatics/"; zdriv="/Volumes/molecular/Molecular"; wmm ="You do not have this path mounted:\n"
+    if(!dir.exists(cbio)){warning(wmm,cbio)}; if(!dir.exists(zdriv)){warning(wmm,zdriv)}
+    message("You have the following drives mounted:")
+    system("ls /Volumes")
+    stopifnot(dir.exists(cbio)|dir.exists(zdriv))
+    system("gcc --version")
+    # .libPaths("/Volumes/CBioinformatics/Methylation/in_house_libs")
+    loadPacks()
+    ms1<- paste0(crayon::bgRed("Your mnp.v11b6 package is outdated execute the function:\n"),
+                 crayon::bgBlue("install.or.load(pathtoFile=NULL, instNew=F, rmpkg=F)\n"),
+                 crayon::bgRed("To update the in-house classifier to current clinical version"))
+    ms2<- paste0(crayon::bgRed("mnp.v11b6 package is not installed execute the function:\n"),
+                 crayon::bgBlue("install.or.load()\n"), crayon::bgRed("to install in-house classifier"))
+    gh.inst("rmarkdown",'rstudio/rmarkdown')
+    if (rq("mgmtstp27")){srcInst(mgmtLn)}
+    if (rq("sest")){sw(srcInst(sexEst))}
+    if (rq("mnp.v11b6")){cat(ms1)}
+    if (!rq("mnp.v11b6")){cat(ms2)}
+    message("\nIf any compiler errors occur run fixCompiles()")
+    #file.path(system.file('data', package = "mnp.v11b6"),'rfpred.v11b6.rds')
+  }else{loadPacks()}
+}
+message("Run setRunType() to get started")
