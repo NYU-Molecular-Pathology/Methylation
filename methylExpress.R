b6184ea150aa4aa8f81dafa59315cd4295a3c2a1
jonserr <jnsrro@gmail.com>
2021-07-12 08:35:01 -0400

    Update methylExpress.R
    added a check for NULL token

--- methylExpress.R
+++ methylExpress.R
@@ -1,10 +1,15 @@
 #!/usr/bin/env Rscript
 args <- commandArgs(TRUE)
 library(base)
+
 # Main Parameters
-token <- paste0(args[1])
-runID <- paste0(args[2]) # Set to NULL to get latest
-selectRDs <- ifelse(length(args[3])>0, args[3], NULL)
+token<-args[1]; runID<-args[2]; selectRDs<-args[3]
+if(length(selectRDs)==0){selectRDs=NULL}else{if(is.na(selectRDs))selectRDs=NULL}
+
+# Check Params
+stopifnot(!is.null(token))
+invisible(lapply(base::ls(), function(i){message(i, " = ", get(i), " (",typeof(i),")\n")}))
+cat("~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
 
 # Source Github Scripts
 mainHub = "https://raw.githubusercontent.com/NYU-Molecular-Pathology/Methylation/main/"
@@ -16,4 +21,3 @@ invisible(lapply(scripts, function(i) {devtools::source_url(i)}))
 if(!is.null(runID)){gb$setVar("runID", runID)}
 gb$prepareRun(token) #print(gb$prepareRun)
 gb$startRun(selectRDs)
-
