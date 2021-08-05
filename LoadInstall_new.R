33706d5691796447e89bc50752af28da794245ab
jonserr <jnsrro@gmail.com>
2021-08-04 22:12:58 -0400

    Update LoadInstall_new.R
    added         unloadNamespace("mnp.v11b4")

--- LoadInstall_new.R
+++ LoadInstall_new.R
@@ -344,6 +344,7 @@ startLoadingAll <- function() {
     install.or.load("Methylation_classifier_v11b4/mnp.v11b4")
   } else {
     if (packageVersion("mnp.v11b4") == '0.1.124') {
+        unloadNamespace("mnp.v11b4")
         detach("package:mnp.v11b4", unload = T)
       install.or.load("Methylation_classifier_v11b4/mnp.v11b4", rmpkg = T)
     } else{
@@ -360,6 +361,7 @@ startLoadingAll <- function() {
   } else{
     if (packageVersion("mnp.v11b6") == '0.1.126') {
       cat(ms[1])
+                unloadNamespace("mnp.v11b6")
                 detach("package:mnp.v11b6", unload = T)
       install.or.load(instNew = T, rmpkg = T)
     } else{
