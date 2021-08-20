6b3d8ce8244c2801b11f73a5a938cf4445bff9ed
jonserr <jnsrro@gmail.com>
2021-08-20 17:00:38 -0400

    Add files via upload

---
+++ Research/getRGsetBetas.R
@@ -0,0 +1,87 @@
+# FUN: Obtain RGSet with Probes common to 450K Array ---------------------------
+combine.EPIC.450K <- function(targets, batchFilter=NULL){
+    if(length(targets$Batch)>0){
+        message("Here are your targets$Batch options: ");print(unique(targets$Batch))
+        targets_450k <- subset(targets, targets$Batch != batchFilter)
+        targets_850k <- subset(targets, targets$Batch == batchFilter)
+        if (!anyDuplicated(targets_450k$Basename) == F) {
+            return(print(paste("450K duplicated basenames:", anyDuplicated(targets_450k$Basename))))}
+        if (!anyDuplicated(targets_450k$Basename) == F) {
+            return(print(paste("850K duplicated basenames:", anyDuplicated(targets_850k$Basename))))}
+        else{cat("reading 450K arrays...")
+            RGSet_450k <- minfi::read.metharray.exp(targets = targets_450k, force = T, verbose = T)
+            cat("reading EPIC arrays...")
+            RGSet_850k <- minfi::read.metharray.exp(targets = targets_850k, force = T, verbose = T)
+            cat("Combining common probes...")
+            RGSet <- minfi::combineArrays(RGSet_450k, RGSet_850k, outType = "IlluminaHumanMethylation450k")
+        }
+    }else{
+        RGSet <- minfi::read.metharray.exp(targets = targets, force = T, verbose = T)
+    }
+    return(RGSet)
+}
+
+# Remove Low Quality and select probes using annotations
+cleanUpProbes <- function(RGSet, saNames, targets, getfunorm=F){
+    detP <- detectionP(RGSet)
+    colnames(detP) <- saNames
+    keep <- colMeans(detP) < 0.05
+    RGSet <- RGSet[, keep]
+    targets <- targets[keep,]
+    detP <- detP[, keep]
+    mSetSq <- preprocessQuantile(RGSet)
+    detP <- detP[match(featureNames(mSetSq), rownames(detP)), ]
+    keep <- rowSums(detP < 0.01) == ncol(mSetSq)
+    gset.funnorm <- addSnpInfo(mSetSq[keep, ])
+    gset.funnorm <- dropLociWithSnps(gset.funnorm,snps=c("SBE","CpG"),maf = 0)#drop loci with snps
+    annot = getAnnotation(gset.funnorm) #getting annotation file
+    sex_probes = annot$Name[annot$chr %in% c("chrX", "chrY")] # dropping sex probes
+    gset.funnorm = gset.funnorm[!(rownames(gset.funnorm) %in% sex_probes),]
+    if (getfunorm){return(gset.funnorm)} else {
+        betas <- getBeta(gset.funnorm)
+        colnames(betas) <- saNames
+        return(betas)
+    }
+}
+
+# Returns topVar beta probes instead of all probes
+takeTopVariance <- function(betas, topVar){
+    var_probes <- apply(betas, 1.0, var)
+    select_var <- names(sort(var_probes[topVar], decreasing = T))
+    top_var_beta <- betas[select_var, ]
+    return(top_var_beta)
+}
+
+getSupervise <- function(the_beta, RGSet, topVar=1:10000){
+    condition <- pData(RGSet)$Type
+    dmp <- dmpFinder(the_beta, pheno = condition, type = "categorical")
+    dmp <- cbind(dmp, ID = rownames(dmp))
+    betas_df <- as.data.frame(the_beta)
+    final_sam <- row.names((t(betas_df[row.names(dmp[topVar, ]), ]))) #topVar=1:10000
+    betas_df <- betas_df[, colnames(betas_df) %in% final_sam]
+    betas <- as.matrix(betas_df)
+    return(betas)
+}
+
+# COMABAT batch correction between 2 groups
+batchCorrectBs <- function(betas,RGSet,topVar=NULL, supervise = F) {
+    batch = targets$Batch
+    modcombat = model.matrix(~ 1, data = targets)
+    combat_beta <- sva::ComBat(
+        dat = betas,
+        batch = batch,
+        mod = modcombat,
+        par.prior = T,
+        prior.plots = F
+    )
+    if (supervise == T) {combat_beta <-getSupervise(combat_beta,RGSet)}
+    if (!is.null(topVar)) {
+        var_probes <- apply(combat_beta, 1.0, var)
+        select_var <- names(sort(var_probes[topVar], decreasing = T))
+        top_variable_beta <- combat_beta[select_var,]
+    } else {top_variable_beta <- combat_beta}
+    # fix if any beta >1 or < 0, so impute >1 is 1 and <0 is 0
+    topVarBetas <- ifelse(top_variable_beta < 0, 0, top_variable_beta)
+    topVarBetas <- ifelse(topVarBetas > 1, 1, topVarBetas)
+    return(topVarBetas)
+}
