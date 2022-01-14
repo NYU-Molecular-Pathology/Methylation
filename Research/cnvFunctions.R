5204ef010f96916d0a9c1a8cb36259cdf8c994ad
jonserr <jnsrro@gmail.com>
2022-01-14 10:23:11 -0500

    Update cnvFunctions.R

--- Research/cnvFunctions.R
+++ Research/cnvFunctions.R
@@ -1,3 +1,5 @@
+gb <- globalenv(); assign("gb", gb)
+
 generateSeg <- function(a, b, c) {
     x <- conumee::CNV.fit(a, b, c)
     x <- conumee::CNV.bin(x)
@@ -22,53 +24,62 @@ customCNV <- function (Mset, sex = NULL) {
         } else {
             load(file.path(path,"CNanalysis6_conumee_REF_F.2018-09-19.RData"))
             return(generateSeg(cndata, refF_epic, annoEPICxy))
-        }
+            }
     } else {
         load(file.path(path,"CNanalysis4_conumee_ANNO.vh20150715.RData"))
         cndata <- conumee::CNV.load(Mset)
         if (sex == "Male") {
             load(file.path(path,"CNanalysis4_conumee_REF-M.vh20150715.RData"))
-            return(generateSeg(cndata, refM.data, annoXY))
+             return(generateSeg(cndata, refM.data, annoXY))
         } else {
             load(file.path(path,"CNanalysis4_conumee_REF-F.vh20150715.RData"))
-            return(generateSeg(cndata, refF.data, annoXY))
+             return(generateSeg(cndata, refF.data, annoXY))
         }
     }
 }
 
-writeSegTab <- function(segFile) {
+writeSegTab <- function(segFile, sentrix.ids) {
   if (!file.exists(segFile)) {
     addCols = NULL
     for (i in 1:length(sentrix.ids)) {
       sampleEpic <- sentrix.ids[i]
-      pathEpic <- file.path(getwd(), sampleEpic)
-      RGsetEpic <- minfi::read.metharray(pathEpic, verbose = T, force = T)
-      MsetEpic <- mnp.v11b6::MNPpreprocessIllumina(RGsetEpic, bg.correct = TRUE, normalize = "controls")
-      if (i == 1) {
-        addCols <- T
-      } else
-        (addCols <- F)
-      x <- customCNV(MsetEpic)
-      q <- conumee::CNV.write(x)
-      yy <- data.frame(x@detail$ratio)
-      colnames(yy) <- paste(samplename_data[i])
-      q$ID <- paste(samplename_data[i])
-      q$group <- paste(samGroup[i])
-      write.table(q, file = segFile, append = T, quote=F, sep=",", col.names=addCols, row.names=F)
+        pathEpic <- file.path(getwd(), sampleEpic)
+        RGsetEpic <- read.metharray(pathEpic, verbose = T, force=T)
+        MsetEpic <- mnp.v11b6::MNPpreprocessIllumina(RGsetEpic, bg.correct = TRUE, normalize = "controls")
+        if(i==1){addCols <-T}else(addCols <- F)
+        x <- gb$customCNV(MsetEpic)
+        q <- CNV.write(x)
+        yy <- data.frame(x@detail$ratio)
+        colnames(yy) <- paste(samplename_data[i])
+        q$ID <- paste(samplename_data[i])
+        q$group <- paste(samGroup[i])
+        write.table(q, file = segFile, append = T, quote=F, sep=",", col.names=addCols, row.names=F)
     }
   }
 }
 
 saveClusters <- function(seg_clust_file,segFile){
-    detailVals <- as.data.frame(read.csv(segFile, row.names=NULL))[,c("chrom","loc.start","loc.end", "seg.mean","ID","group")]
-    colnames(detailVals) <- c("chromosome", "start", "end", "segmean", "sample","group")
-    
+    detailVals <- as.data.frame(read.csv(segFile, row.names=NULL))[,c("chrom","loc.start","loc.end", "seg.mean","ID")]
+    colnames(detailVals) <- c("chromosome", "start", "end", "segmean", "sample")
     detailVals$segmean <- (2**(detailVals$segmean))*2
-    sexChr <- detailVals$chromosome=="chrX" | detailVals$chromosome== "chrY"
-    detailVals <- detailVals[!sexChr,]
     write.table(detailVals,file=seg_clust_file,sep = "\t",row.names = F)
     cnData <- read.delim(seg_clust_file,header = T,sep = "\t",row.names=NULL)
     return(cnData)
+}
+
+
+savePlotPdf <- function(cnData, plotName, plotTitle){
+    freqPlot <- suppressMessages(
+      GenVisR::cnFreq(cnData,genome = "hg19",plotType = "frequency", 
+                      plot_title=plotTitle, CN_Loss_colour = "#00B0EB", 
+                      CN_Gain_colour = "#00A36D"))
+    labelTitle <- paste("###", plotName,'\n\n')
+    cat(labelTitle)
+    print(freqPlot)
+    cat('\n\n')
+    pdf(file=paste0(plotName,"_cnv.pdf"),height = 5,width = 25)
+    freqPlot
+    dev.off()
 }
 
 
@@ -98,11 +109,10 @@ savePlotPng <- function(cnData, plotName, plotTitle, plotChr = c(paste0("chr",1:
 }
 
 grabClusterDat <- function(seg_clust_file,segFile){
-if(!file.exists(seg_clust_file)){
-    cnData <- gb$saveClusters(seg_clust_file,segFile)
-}else{
-    cnData <- read.delim(seg_clust_file,header = T,sep = "\t",row.names=NULL)
-}
+cnData <- if(!file.exists(seg_clust_file)){
+  gb$saveClusters(seg_clust_file,segFile)} else{
+    read.delim(seg_clust_file,header = T,sep = "\t",row.names=NULL)
+    }
 return(cnData)
 }
 
