d2d9a603f5a0cf8d9dc20c2efda96d43b97a224d
jonserr <jnsrro@gmail.com>
2022-02-15 16:07:25 -0500

    Update tableMaker.R
    replaced package requires with librarian package

--- Research/tableMaker.R
+++ Research/tableMaker.R
@@ -5,37 +5,44 @@ setDirectory<-function(foldr) {
 }
 
 #knitr::opts_chunk$set(fig.width=15, fig.height=10)
-library("foreach")
-require("foreach")
-library("dplyr")
-library("kableExtra")
-if(!require("GenVisR")){BiocManager::install("GenVisR")}
-library("minfi")
+
+if(!require("librarian")){install.packages("librarian", dependencies=T, verbose=T, Ncpus = 6, quiet=T)}
+pkgs <- c(
+    "foreach",
+    "BiocManager",
+    "dplyr",
+    "DT",
+    "kableExtra",
+    "foreach",
+    "minfi",
+    "magick",
+    "conumee",
+    "mnp.v11b6",
+    "enrichplot",
+    "limma",
+    "stringr",
+    "tidyverse",
+    "tidyr",
+    "DOSE",
+    "ggnewscale",
+    "enrichplot",
+    "ComplexHeatmap",
+    "assertr",
+    "Biobase",
+    "magick",
+    "irlba",
+    "magick",
+    "grid",
+    "Cairo",
+    "gridExtra"
+)
+librarian::shelf(pkgs, ask=F, warn.conflicts=F)
+
+require("gridExtra")
+require("Cairo")
 require("minfi")
-library("conumee")
-library("mnp.v11b6")
-library("enrichplot")
-library("limma")
-library("stringr")
-library(tidyverse)
-require(tidyr)
-library("DOSE")
-library(ggnewscale)
-library("enrichplot")
-require("enrichplot")
-if(!require("assertr")){install.packages("assertr")}
-suppressPackageStartupMessages(library(ComplexHeatmap))
 require("ComplexHeatmap")
-library("Biobase")
-if(!require("magick")){install.packages("magick", quiet =T)}
-if(!require("irlba")){install.packages("irlba", quiet =T)}
-library(magick)
-library(grid)
-require(grid)
-library(gridExtra)
-require(gridExtra)
-library(Cairo)
-require(Cairo)
+if(!require("GenVisR")){BiocManager::install("GenVisR")}
 
 makeDt <- function(targets) {
   dtOpts <- list(scrollX = T, scrollY = T, info = F, pageLength=15,autoWidth = T, rownames = F,lengthChange = T,searchable = T)
