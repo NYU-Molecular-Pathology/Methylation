a01a14f5c4ca697f581120115d1c69cfc0864dba
jonserr <jnsrro@gmail.com>
2022-02-17 12:59:34 -0500

    Update TsnePlotter.R
    updated and annotated genTsnePlot function

--- Research/TsnePlotter.R
+++ Research/TsnePlotter.R
@@ -76,75 +76,70 @@ generateTvals <- function(betas) {
     )
 }
 
-genTsnePlot <- function(tsne_plot, titleLabel, groupToLabel = NULL, symbolsLabel=NULL, colorLabel=NULL,
-                        names2Label=NULL){
-    col_vect <- pals::glasbey()
-    col_vect[6] = "#eb7d34" #changing dark forest to orange color
-    colours <- col_vect[1:(length(unique(tsne_plot$GROUPS)))]
+genTsnePlot <- function(tsne_plot, titleLabel, groupToLabel = NULL, symbolsLabel=NULL, colorLabel=NULL, names2Label=NULL){
+    colours <- unique(tsne_plot$col)
     symFlags <- !is.null(symbolsLabel)
     #options("device.ask.default"=F)
     devAskNewPage(ask=F)
-    if(symFlags==T){
-        shapeVals <- c(17, 19, 15, 7, 8, 9, 1, 3, 4, 5)
-        sv <- shapeVals[1:length(unique(tsne_plot$symbol))]
-        shapeLabels <- levels(as.factor(tsne_plot$symbol)) #"Sample Type"
-        if(any(is.na(shapeLabels))){shapeLabels<-colorLabel}
-        symShape <- as.factor(tsne_plot$symbol)
-    }else{symShape <- shapeVals <- shapeLabels <- NULL}
-    et <- element_text(size = 24)
+    # Assigns symbols to each unique sample type
+    if(symFlags==T) {
+      shapeVals <- c(17, 19, 15, 7, 8, 9, 1, 3, 4, 5)
+      sv <- shapeVals[1:length(unique(tsne_plot$symbol))]
+      shapeLabels <- levels(as.factor(tsne_plot$symbol)) #"Sample Type"
+      if (any(is.na(shapeLabels))) {shapeLabels <- colorLabel}
+      symShape <- as.factor(tsne_plot$symbol)
+    } else{
+        symShape <- shapeVals <- shapeLabels <- NULL
+        }
+    # Text size set
+    et <- element_text(size = 18)
+    # Theme parameters for ggrepel
+    labelParams <- as.list(
+      alpha = 0.85, segment.alpha = 0.70, nudge_x = -30, nudge_y = 4,
+      direction = "both", fontface = "bold", box.padding = 0.5, 
+      fill = "pink", min.segment.length = 0.01, color = "black",
+      label.size = 1.0, size = 3, label.padding = unit(0.5, "lines"),
+      label.r = unit(0.5, "lines"), force = 8, show.legend = F
+    )
+    # Main Plot
     groupTsne <- ggplot(tsne_plot,aes(x=tsne_plot$x,y=tsne_plot$y,group=tsne_plot$GROUPS)) +
-        geom_point(aes(x,y,color=tsne_plot$GROUPS, shape= symShape), size=4,alpha=0.85)
+        geom_point(aes(x, y, color=tsne_plot$GROUPS, shape=symShape), size=4, alpha=0.85)
     if(symFlags==T){
         groupTsne <- groupTsne + 
-            scale_shape_manual(name="Sample Type",values=sv,labels=shapeLabels)
+          scale_shape_manual(name="Sample Type" , values=sv, labels=shapeLabels)
     }
+     # Adding manual plot colors and titles
     groupTsne <- groupTsne +
-        scale_color_manual(values=colours, name= "Sample Label") +
-        labs(color = colorLabel, size = 4) + theme_bw(base_size = 24) +
-        theme(
+      scale_color_manual(values=colours, name= "Sample Label") +
+      labs(color = colorLabel, size = 4) + theme_bw(base_size = 24) +
+      theme(
             panel.grid.major = element_blank(),
             panel.grid.minor = element_blank(),
             panel.background = element_blank(),
             text = et
-        ) +
-        ggtitle(label = titleLabel) +
-        theme(
-            plot.title = et,
-            legend.text = et,
-            text = et,
-            legend.position = "right",
-            axis.text.y = et,
-            axis.text.x = et
-        ) +
-        guides(fill = guide_legend(title = "Sample Legend")) +
-        labs(x = "TSNE 1", y = "TSNE 2")
+            ) +
+      ggtitle(label = titleLabel) +
+      theme(plot.title = et, legend.text = et, text = et,
+            legend.position = "right", 
+            axis.text.y = et, axis.text.x = et) +
+      guides(fill = guide_legend(title = "Sample Legend")) + 
+      labs(x = "TSNE 1", y = "TSNE 2")
     # Below only runs to label sample IDs if label group is provided
     if (!is.null(groupToLabel)) {
         groupTsne <- groupTsne + ggrepel::geom_label_repel(
             data = subset(tsne_plot, tsne_plot$samples == groupToLabel),
-            aes(x = x,  y = y, label = samples, size = 2),
-            alpha = 0.85, segment.alpha = 0.70, nudge_x = -30, nudge_y = 4,
-            direction = "both", fontface = "bold",
-            box.padding = 0.5, fill = "pink", min.segment.length = 0.01, color = "black",
-            label.size = 1.0, size = 4, label.padding = unit(0.5, "lines"),
-            label.r = unit(0.5, "lines"), force = 8, show.legend = F)
+            aes(x = x, y = y, label = samples, size = 2), labelParams)
     }
-    
+    # Below only runs if specific sample names are provided
     if (!is.null(names2Label)) {
         groupTsne <- groupTsne + ggrepel::geom_label_repel(
             data = tsne_plot,
-            aes(x = x,  y = y, label = samples, size = 2),
-            alpha = 0.85, segment.alpha = 0.70, nudge_x = -30, nudge_y = 4,
-            direction = "both", fontface = "bold",
-            box.padding = 0.5, fill = "pink", min.segment.length = 0.01, color = "black",
-            label.size = 1.0, size = 4, label.padding = unit(0.5, "lines"),
-            label.r = unit(0.5, "lines"), force = 8, show.legend = F)
+            aes(x = x, y = y, label = samples, size = 2), labelParams)
     }
     return(groupTsne)
 }
 
-
- getTsneVal <- function(TSNE, saNames, samGrp, colorGrp, symGrp) {
+getTsneVal <- function(TSNE, saNames, samGrp, colorGrp, symGrp) {
         tsne_plot <- data.frame(
             x = TSNE$Y[, 1],
             y = TSNE$Y[, 2],
