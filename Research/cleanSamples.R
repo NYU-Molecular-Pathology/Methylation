02cfcee216a4668ba7454123f23eda9ad7e8e827
jonserr <jnsrro@gmail.com>
2022-05-12 16:02:52 -0400

    Update cleanSamples.R

--- Research/cleanSamples.R
+++ Research/cleanSamples.R
@@ -2,8 +2,8 @@ writeTargets <- function(targets, csvFi="samplesheet.csv"){
   write.csv(targets, file, quote=F, row.names=F)
 }
 
-
 setKeyColumns <- function(targets, col_samTypes, col_samNames, col_other){
+  if(class(targets)!="data.frame"){targets<- as.data.frame(targets)}
   targets$Type <- targets[ , col_samTypes] # Creates any new "Type" column
   targets$Sample_Name <- targets$Sample_ID <- targets[ , col_samNames] # generates Sample_ID column if doesn't exist
   targets$Sample_Group <- targets[,col_other]
