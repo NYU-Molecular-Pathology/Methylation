387f9243cd3a650db7274c92942adbca0b8302b6
jonserr <jnsrro@gmail.com>
2022-06-17 15:59:56 -0400

    Update pullRedcap_manual.R

--- Research/pullRedcap_manual.R
+++ Research/pullRedcap_manual.R
@@ -72,10 +72,9 @@ search.redcap <- function(rd_numbers, token=NULL, flds=NULL) {
 # FUN: Sets your directory and sources the helper functions
 sourceFuns <- function(workingPath = NULL) {
     mainHub = "https://raw.githubusercontent.com/NYU-Molecular-Pathology/Methylation/main/"
-    script.list <- c("SetRunParams.R","CopyInputs.R","PACT_scripts/generateCNV.R")
+    script.list <- c("R/SetRunParams.R","R/CopyInputs.R","PACT_scripts/generateCNV.R")
     if (is.null(workingPath)) {workingPath = getwd()}
     scripts <- paste0(mainHub, script.list)
-    
     invisible(lapply(scripts, function(i){devtools::source_url(i)}))
     gb$setDirectory(workingPath)
     return(gb$defineParams(loadClassifier=F))
