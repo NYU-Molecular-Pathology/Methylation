b0a8ef89d0b1e073cc0e7027ed11810e121ada61
jonserr <jnsrro@gmail.com>
2022-07-18 16:39:14 -0400

    edited comments and moved parameters together

--- R/methylExpress.R
+++ R/methylExpress.R
@@ -3,16 +3,16 @@ args <- commandArgs(TRUE)
 library("base")
 gb <- globalenv(); assign("gb", gb)
 
-if(!require("devtools")){install.packages("devtools")}
+if(!require("devtools")){install.packages("devtools", quiet=T)}
 
-# Main Parameters trailing commandline
+# Parameters Input trailing commandline ----------------------------------------------------
 token <-      args[1]
 runID <-      args[2]
 selectRDs <-  args[3]
 baseFolder <- args[4] #NULL
 redcapUpload <- args[5]
 
-# Check Parameters Input
+# Check Input Parameters ----------------------------------------------------
 if(length(selectRDs)==0 | identical(selectRDs,NULL) | identical(selectRDs,"NULL")
 ){selectRDs=NULL}else {if(is.na(selectRDs)){selectRDs=NULL}}
 
@@ -29,10 +29,9 @@ if(length(redcapUpload)==0 | identical(redcapUpload,NULL) | identical(redcapUplo
   }
 }
 
-# Check Input Params
+# Message input on console
 message("\n~~~~~~~~~~~~~~~~~~~~~Parameters input~~~~~~~~~~~~~~~~~~~~~\n")
-message("token: ", token,"\nrunID: ", runID,"\nselectRDs: ", selectRDs, "\nbaseFolder: ", baseFolder, "\nredcapUpload: ", redcapUpload)
-message("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+message("token: ", token,"\nrunID: ", runID,"\nselectRDs: ", selectRDs, "\nbaseFolder: ", baseFolder, "\nredcapUpload: ", redcapUpload, "\n")
 
 # Cancel if no token or runID
 stopifnot(!is.null(token)); stopifnot(!is.null(runID))
@@ -41,44 +40,34 @@ if(!is.null(baseFolder) & !identical(baseFolder,NULL)){
     message("Trying custom run directory from input:","\n", baseFolder,"\n")
     isValid <- dir.exists(baseFolder)
     message("Checking if directory exists: ", isValid)
-    if(isValid==F){message("Directory does not exist, trying to create path:\n", baseFolder,"\n")
-        tryCatch(
-            expr={dir.create(baseFolder)},
-            warning=function(er){message("\n","One warning during directory creation","\n",er)},
-            error=function(er){message("\n","An Error during directory creation:","\n",er,"\n")},
-            finally={stopifnot(dir.exists(baseFolder))}
-        )
+    if(isValid==F){
+        message("Directory provided does not exist, try creating it first:\n","mkdir ", baseFolder,"\n")
+        stopifnot(isValid==TRUE)
     }
 }else {baseFolder=NULL}
 
-# Paths to the GitHub Repo files
+
+# Source GitHub Scripts ----------------------------------------------------
 mainHub = "https://raw.githubusercontent.com/NYU-Molecular-Pathology/Methylation/main/R/"
 script.list = c("LoadInstall_new.R","SetRunParams.R","MakeSampleSheet.R", "CopyInputs.R","CopyOutput.R", "pipelineHelper.R")
-
-# Source GitHub Scripts
 scripts = paste0(mainHub, script.list)
 invisible(lapply(scripts, function(i) {devtools::source_url(i)}))
-# Define any custom directories here, default is NULL
-#mnp.pk.loc = NULL, ApiToken = NULL, methDir = NULL, clinDrv = NULL, rschOut = NULL, clinOut = NULL,
-#rsch.idat = NULL, clin.idat = NULL, QC_file = NULL, isMC = T, baseDir = NULL, runID = NULL
 
+# Define Parameters ----------------------------------------------------
 gb$defineParams(
     methDir = baseFolder,
     baseDir = baseFolder,
     ApiToken = token
 )
+gb$setVar("runID", runID)
 
-# Execute Methylation Run
-if(!is.null(runID)){gb$setVar("runID", runID)}
-
-
+# Functions ----------------------------------------------------
 checkBaseFolder <- function(baseFolder){
     if(is.null(baseFolder)){
         gb$baseFolder <- "/Volumes/CBioinformatics/Methylation/Clinical_Runs"
         gb$methDir <- "/Volumes/CBioinformatics/Methylation/Clinical_Runs"
         gb$baseDir <- "/Volumes/CBioinformatics/Methylation/Clinical_Runs"
     }
-
     if(length(baseFolder)>0){
         if(stringr::str_detect(baseFolder, pattern="Desktop")==T){
             warning("Trying to run methylation from Desktop working directory is not allowed")
@@ -86,24 +75,26 @@ checkBaseFolder <- function(baseFolder){
             stopifnot(stringr::str_detect(baseFolder, pattern="Desktop")==F)
         }
     }
-
     return(baseFolder)
 }
 
-# Executes the functions in order to setup a run
-gb$prepareRun <- function(token, baseFolder=NULL, runLocal=F){
-    baseFolder <- checkBaseFolder(baseFolder)
-    if(runLocal==F){
-        gb$checkMounts()
-        gb$checkValidRun(gb$runID)
-
+# Sets the working folder directory
+SetBaseFolder <- function(token, baseFolder){
     methylPath <- gb$setRunDir(gb$runID, workFolder = baseFolder)
     message("Working directory set to:\n", crayon::bgGreen(methylPath), "\n")
     gb$workFolder <- baseFolder
     gb$setVar("workFolder", baseFolder)
-    setwd(methylPath)
     gb$setVar("ApiToken", token) # assign the ApiToken & print params
+}
 
+# Executes the functions in order to setup a run
+PrepareRun <- function(token, baseFolder=NULL, runLocal=F){
+    if(runLocal==F){
+        gb$checkMounts()
+        gb$checkValidRun(gb$runID)
+    }
+    baseFolder <- checkBaseFolder(baseFolder)
+    SetBaseFolder(token, baseFolder)
     if(runLocal==F) {
         gb$copyWorksheetFile(runID = gb$runID) # copies the xlsm file
         gb$readSheetWrite() # reads xlsm and generates input .csv samplesheet
@@ -111,26 +102,30 @@ gb$prepareRun <- function(token, baseFolder=NULL, runLocal=F){
         gb$moveSampleSheet(gb$methDir) #copies outputs temp to desktop for QC.Rmd
     } else{
         gb$readSheetWrite()
+        gb$moveSampleSheet()
     }
 }
 
-gb$prepareRun(token, baseFolder)
+# Starts run knitting html reports loop
+StartRun <- function(selectRDs = NULL, emailNotify = T, redcapUp = T) {
+    gb$msgFunName(paste0(mainHub,"methylExpress.R"),"startRun")
+    message("Default Params: selectRDs=NULL, emailNotify=T, redcapUp=T")
+    gb$msgParams(selectRDs, emailNotify, redcapUp)
+
+    # Re-order sample report generation for priority
+    if (!is.null(selectRDs)) {runOrder <- gb$reOrderRun(selectRDs) }else{runOrder <- NULL}
+    gb$makeReports.v11b6(
+        skipQC = F,            # Don't skip QC generation
+        email = emailNotify,   # to email after Run complete
+        cpReport = T,          # Flag to copy files to network drive
+        selectSams = runOrder, # Prioritize specific RD-numbers
+        redcapUp = redcapUp    # Flag to import files to REDCap
+    )
+}
 
 if(!is.null(selectRDs)){selectRDs <- stringr::str_split(selectRDs, ",")}
-
-gb$startRun <- function(selectRDs=NULL, emailNotify=T, redcapUp=T){
-    gb$msgFunName("https://github.com/NYU-Molecular-Pathology/Methylation/edit/main/methylExpress.R","startRun")
-    message("Default Params: selectRDs=NULL, emailNotify=T")
-    nullVal = ifelse(is.null(selectRDs),"NULL", selectRDs)
-    gb$msgParams(nullVal, emailNotify)
-
-    if(!is.null(selectRDs)){
-        sampleOrder <- gb$reOrderRun(selectRDs) # Re-order sample report generation for priority
-        gb$makeReports.v11b6(skipQC=F, email=emailNotify, cpReport=T, selectSams=sampleOrder, redcapUp=T)
-    } else {
-        gb$makeReports.v11b6(skipQC=F, email=emailNotify, cpReport=T, selectSams=NULL, redcapUp=T)
-    }
-}
-
 assign("redcapUpload", redcapUpload)
-gb$startRun(selectRDs, emailNotify=T,redcapUp=T) # can change to default false
+
+# Execute Functions ----------------------------------------------------
+PrepareRun(token, baseFolder) # If running local and  runLocal = TRUE
+StartRun(selectRDs, emailNotify=T,redcapUp=T) # can change to default false
