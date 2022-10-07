86ee9fe6aa518d2ca1f48e611246404a36e81489
jonserr <jnsrro@gmail.com>
2022-10-07 17:52:34 -0400

    Update ClassTables.R

--- R/Report-Scripts/ClassTables.R
+++ R/Report-Scripts/ClassTables.R
@@ -371,3 +371,19 @@ GetClassProbTables <- function(out_class_family=NULL, outList=NULL){
     
     return(list("famTable"=famTable,"grpTable"=grpTable))
 }
+                      
+SuppInfoTable <- function(suppinfo){
+    suppinfo <- as.data.frame(t(suppinfo))
+    rownames(suppinfo) <- NULL
+    kgb <- c("striped",font_size = 9, bootstrap_options = c("bordered"), position = "float_left")
+    suppTab <- suppinfo %>% 
+    	knitr::kable("html",c(booktabs = T, escape = F, linesep = ""), align='clc') %>%
+    	kableExtra::kable_styling(kgb, full_width = F, position="left") %>% 
+        column_spec(column = c(1:5), width = "200px") %>%
+        column_spec(1, border_left = "3px solid white") %>%
+        column_spec(5, border_right = "3px solid white") %>%
+        kableExtra::row_spec(row = 0, font_size = 12, background="rgb(255, 235, 205)", color = "black") %>%
+        kableExtra::row_spec(row = 0, background="rgb(255, 235, 205)") %>%
+        kableExtra::row_spec(row = 1, font_size = 11)
+    return(suppTab)
+}
