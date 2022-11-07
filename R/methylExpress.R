08407377abfebf186ebb036d75fef61b2261dfc4
jonserr <jnsrro@gmail.com>
2022-11-07 13:53:13 -0500

    Update methylExpress.R

--- R/methylExpress.R
+++ R/methylExpress.R
@@ -48,7 +48,7 @@ if(!is.null(baseFolder) & !identical(baseFolder, "NULL")) {
 
 # Source GitHub Scripts ----------------------------------------------------
 mainHub = "https://raw.githubusercontent.com/NYU-Molecular-Pathology/Methylation/main/R/"
-script.list = c("LoadInstallPackages.R", "SetRunParams.R", "MakeSampleSheet.R", "CopyInputs.R", "CopyOutput.R", "pipelineHelper.R")
+script.list = c("LoadInstallPackages.R", "SetRunParams.R", "MakeSampleSheet.R", "CopyInputs.R", "CopyOutput.R", "pipelineHelper.R", "CustomRuns.R")
 scripts = paste0(mainHub, script.list)
 lapply(scripts, function(i){message("Sourcing: ", i); devtools::source_url(i)})
 
@@ -57,8 +57,7 @@ selectRDs <- AssignArgs(runID, baseFolder, token, selectRDs, redcapUp, gb)
 require(compiler)
 enableJIT(3)
 mainPage = "https://raw.githubusercontent.com/NYU-Molecular-Pathology/Methylation/main/R/Report-Scripts/"
-scripts <- c("ClassTables.R", "MLH1_Functions.R", "PipeLineU.R",
-             "RedcapOutput.R", "TsneFunctions.R", "cnvggplotly.R")
+scripts <- c("ClassTables.R", "MLH1_Functions.R", "PipeLineU.R", "RedcapOutput.R", "TsneFunctions.R", "cnvggplotly.R")
 scripts <- paste0(mainPage, scripts)
 lapply(scripts, function(i) {message("Sourcing: ", i);devtools::source_url(i)})
 
