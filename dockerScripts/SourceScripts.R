65964e34809568bd056a8ab42ca8cbc43a0d351b
jonserr <jnsrro@gmail.com>
2022-11-15 16:17:40 -0500

    Update SourceScripts.R

--- dockerScripts/SourceScripts.R
+++ dockerScripts/SourceScripts.R
@@ -5,7 +5,7 @@ enableJIT(3)
 
 cbioLn <- switch (Sys.info()[['sysname']],
                   "Darwin" = "/Volumes/CBioinformatics/Methylation/classifiers",
-                  "Linux" = "~/molecpathlab/production/Methylation/classifiers")
+                  "Linux" = "./private_pkgs")
 
 uniDpath <- file.path(cbioLn, "UniD")
 
@@ -379,22 +379,6 @@ loadPacks <- function(pkgs=cranPkgs, ezLibs=easyPkgs, ghPk=gHubPkgs, bcPks=biocP
     )
 }
 
-
-runAllBrew <- function(){
-    # export RSTUDIO_WHICH_R=/usr/local/bin/R
-    system("brew install aspell gdal autoconf automake bash cairo cmake coreutils dos2unix exa fd fontconfig freetype fribidi fzf gawk gcc gdbm gdk-pixbuf gettext giflib git glib gmp gnu-sed gnu-tar gnutls graphite2 harfbuzz htop icu4c ilmbase imagemagick isl jpeg libcroco libde265 libevent libffi libgcrypt libgpg-error libheif libidn2 libmpc libomp libpng librsvg libtasn1 libtermkey libtiff libtool libunistring libuv libvterm libyaml little-cms2 llvm lua lua@5.1 luajit luarocks lzo mpfr msgpack multimarkdown ncurses neovim nettle nspr nss oniguruma openconnect openexr openjpeg openssl@1.1 p11-kit pandoc pandoc-citeproc pandoc-crossref pango pcre pcre2 pdfgrep perl pixman pkg-config poppler qt readline rename ripgrep ruby shared-mime-info shellcheck sqlite stoken swig texinfo tmux tree unbound unibilium vim webp wget x265")
-    system('export CFLAGS+=-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk
-  export CCFLAGS+=-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk
-  export CXXFLAGS+=-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk
-  export CPPFLAGS+=-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk')
-    system("brew install golang libstfl")
-    if(dir.exists("/Library/Developer/CommandLineTools")){
-        if(length(list.dirs("/Library/Developer/CommandLineTools", recursive = F))>0){
-            system("ln -s /Applications/Xcode.app/Contents/Developer /Library/Developer/CommandLineTools")}}
-    system("brew unlink gcc && brew link gcc --force")
-    system('brew link --overwrite gcc')
-    system('brew doctor')
-}
 
 colorMsg <- function(){
     ms1 <- paste0(mkGrn("Updating in-house classifier to current version:"),"\n",
