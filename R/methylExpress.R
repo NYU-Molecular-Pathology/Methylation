ed182656029af16b768792523c542b3ebe7ab2f9
jonserr <jnsrro@gmail.com>
2022-11-22 16:13:17 -0500

    updated documentation and code structure for legibility

--- R/methylExpress.R
+++ R/methylExpress.R
@@ -1,24 +1,24 @@
 #!/usr/bin/env Rscript
-args <- commandArgs(TRUE)
 library("base")
 gb <- globalenv(); assign("gb", gb)
+args <- commandArgs(TRUE)
 
-if(!require("devtools")){install.packages("devtools", quiet=T)}
+# Parameters Input trailing commandline -------------------------------------------------------
+args[1] -> token      # <- NULL
+args[2] -> runID      # <- 21-MGDM_TEST
+args[3] -> selectRDs  # <- NULL
+args[4] -> baseFolder # <- NULL
+args[5] -> redcapUp   # <- TRUE
+args[6] -> runLocal   # <- FALSE
 
-# Parameters Input trailing commandline ----------------------------------------------------
-args[1] -> token
-args[2] -> runID
-args[3] -> selectRDs
-args[4] -> baseFolder
-args[5] -> redcapUp
-args[6] -> runLocal
-
+# Check BigPurple Enironment & Devtools -------------------------------------------------------
 if(Sys.info()[['sysname']]=="Linux"){
   .libPaths("~/molecpathlab/production/Methylation/common_libs")
   assign("baseFolder", "~/molecpathlab/production/Methylation", envir = gb)
 }
+if(!require("devtools")){install.packages("devtools", quiet=T)}
 
-# Check Input Parameters ----------------------------------------------------
+# Check Input Parameters -----------------------------------------------------------------------
 CheckInputArg <- function(varValue, gb, defVal = NULL) {
     varStr <- deparse(substitute(varValue))
     if (length(varValue) == 0 | identical(varValue, NULL) | identical(varValue, "NULL")) {
@@ -28,45 +28,52 @@ CheckInputArg <- function(varValue, gb, defVal = NULL) {
     return(assign(varStr, varValue, envir = gb))
 }
 
+# Message and Check Input Args ------------------------------------------------------------------
 message("\n~~~~~~~~~~~~~~~~~~~~~Parameters input~~~~~~~~~~~~~~~~~~~~~")
-CheckInputArg(token, gb)
-CheckInputArg(runID, gb)
-CheckInputArg(selectRDs, gb)
-CheckInputArg(baseFolder, gb)
-CheckInputArg(redcapUp, gb, T)
-CheckInputArg(runLocal, gb, F)
-message("\n^^^^^^^^^^^^^^^^^^^^^Parameters input^^^^^^^^^^^^^^^^^^^^^\n")
+CheckInputArg(token, gb); CheckInputArg(runID, gb)
+CheckInputArg(selectRDs, gb); CheckInputArg(baseFolder, gb)
+CheckInputArg(redcapUp, gb, T); CheckInputArg(runLocal, gb, F)
 
-# Cancel if no token or runID ----------------------------------------------------
+# Cancel if no token or runID -------------------------------------------------------------------
 stopifnot(!is.null(token)); stopifnot(!is.null(runID))
 if(!is.null(baseFolder) & !identical(baseFolder, "NULL")) {
     message("Checking if custom run directory is valid: ", baseFolder, "\n")
-    stopifnot("Directory input does not exist! Try creating it first with mkdir" = dir.exists(baseFolder) == T)
+    stopifnot("Input directory does not exist! Create it with mkdir" = dir.exists(baseFolder) == T)
 } else{baseFolder <- NULL}
 
-# Source GitHub Scripts ----------------------------------------------------
-mainHub = "https://raw.githubusercontent.com/NYU-Molecular-Pathology/Methylation/main/R/"
-script.list = c("LoadInstallPackages.R", "SetRunParams.R", "MakeSampleSheet.R", "CopyInputs.R", "CopyOutput.R", "pipelineHelper.R", "CustomRuns.R")
-scripts = paste0(mainHub, script.list)
-lapply(scripts, function(i){message("Sourcing: ", i); devtools::source_url(i)})
+LoadGitHubScripts <- function(ghRepo, scriptList){
+  scripts = file.path(ghRepo, scriptList)
+  return(lapply(scripts, function(i){message("Sourcing: ", i); devtools::source_url(i)}))
+}
 
-# Define Parameters ----------------------------------------------------
-selectRDs <- AssignArgs(runID, baseFolder, token, selectRDs, redcapUp, gb)
-require(compiler)
-enableJIT(3)
-mainPage = "https://raw.githubusercontent.com/NYU-Molecular-Pathology/Methylation/main/R/Report-Scripts/"
-scripts <- c("ClassTables.R", "MLH1_Functions.R", "PipeLineU.R", "RedcapOutput.R", "TsneFunctions.R", "cnvggplotly.R")
-scripts <- paste0(mainPage, scripts)
-lapply(scripts, function(i) {devtools::source_url(i); return(message("Sourced: ", i))})
+# Source GitHub Scripts -------------------------------------------------------------------------
+mainHub = "https://raw.githubusercontent.com/NYU-Molecular-Pathology/Methylation/main/R"
+scriptList <-
+  c(
+    "LoadInstallPackages.R",
+    "SetRunParams.R",
+    "MakeSampleSheet.R",
+    "CopyInputs.R",
+    "CopyOutput.R",
+    "pipelineHelper.R",
+    "CustomRuns.R"
+  )
 
-unloadNamespace("mnp.v11b4")
-unloadNamespace("mnp.v12b6")
-unloadNamespace("sarc.v12b6")
-loadNamespace("mnp.v11b6")
-require("mnp.v11b6")
-library("mnp.v11b6")
+# Source Additional Scripts for Rmd Knit ---------------------------------------------------------
+rmdScripts <- c("ClassTables.R", "MLH1_Functions.R", "PipeLineU.R",
+                "RedcapOutput.R", "TsneFunctions.R", "cnvggplotly.R")
 
-# Execute Functions ----------------------------------------------------
-gb$PrepareRun(token, baseFolder, runID, runLocal=runLocal) # If running local and  runLocal = TRUE
+LoadGitHubScripts(mainHub, scriptList)
+LoadGitHubScripts(file.path(mainHub,"Report-Scripts"), rmdScripts)
+
+# Unload Libraries to properly source v11 Package References -------------------------------------
+unloadNamespace("mnp.v11b4"); unloadNamespace("mnp.v12b6"); unloadNamespace("sarc.v12b6")
+loadNamespace("mnp.v11b6"); require("mnp.v11b6"); library("mnp.v11b6")
+
+# Assign Parameters if Defined -------------------------------------------------------------------
+selectRDs <- gb$AssignArgs(runID, baseFolder, token, selectRDs, redcapUp, gb)
 gb$reportMd <- reportMd <- "~/report.Rmd"
-gb$StartRun(selectRDs, emailNotify=T, redcapUp=redcapUp) # can change to default false
+
+# Execute Pipeline Functions ----------------------------------------------------------------------
+gb$PrepareRun(token, baseFolder, runID, runLocal=runLocal) # If running local set runLocal = TRUE
+gb$StartRun(selectRDs, emailNotify=T, redcapUp=redcapUp) # Can be changed to default false
