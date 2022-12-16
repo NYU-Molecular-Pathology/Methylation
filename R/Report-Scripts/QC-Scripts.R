9e22ea3e82f17ef3e47c4bc0b0ac153241c2e6b0
jonserr <jnsrro@gmail.com>
2022-12-15 19:42:39 -0500

    Update QC-Scripts.R

--- R/Report-Scripts/QC-Scripts.R
+++ R/Report-Scripts/QC-Scripts.R
@@ -210,7 +210,6 @@ getProbes <- function(probeName) {
 
 ## Merges Dataframe Values --------------------------------------
 mergeDF <- function(df, tg) {
-	rownames(tg) <- tg$SentrixID_Pos
 	mdf <- merge(df, tg, by = "row.names", suffixes = c("", ".y"))
 	return(mdf)
 }
