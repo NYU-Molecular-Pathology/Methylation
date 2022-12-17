db4860443fb6fa9acdfeb9e6fb95bd28add21adb
jonserr <jnsrro@gmail.com>
2022-12-17 16:24:23 -0500

    Update DockerPrePkgs.R

--- dockerScripts/DockerPrePkgs.R
+++ dockerScripts/DockerPrePkgs.R
@@ -1,20 +1,7 @@
-update.packages(
-    ask = FALSE,
-    contriburl = contrib.url(
-        'https://packagemanager.rstudio.com/all/__linux__/focal/latest'
-    )
-)
-
-if (!require('rJava')) {
-    install.packages(lib = '/usr/local/lib/R/site-library/', 'rJava')
-}
-
-install.packages(lib = '/usr/local/lib/R/site-library/', 'pak',
-                 repos = 'https://r-lib.github.io/p/pak/dev/', dependencies = T)
-
-options(repos = 'https://packagemanager.rstudio.com/all/__linux__/focal/latest')
-pak::pkg_install(
-    c(
+update.packages( contriburl = contrib.url('https://packagemanager.rstudio.com/all/__linux__/focal/latest'), ask = F)
+if (!require('rJava')) {install.packages(lib = '/usr/local/lib/R/site-library/', 'rJava')}
+install.packages(lib = '/usr/local/lib/R/site-library/', 'pak', repos = 'https://r-lib.github.io/p/pak/dev/', dependencies = T)
+basePkg <- c(
         'shiny',
         'rmarkdown',
         'Hmisc',
@@ -25,20 +12,10 @@ pak::pkg_install(
         'curl',
         'httr',
         'xml2'
-    ), dependencies = T, ask = F 
-)
-pak::pkg_install(
-    c('devtools', 'BiocVersion', 'BiocManager'),
-    dependencies = T,
-    ask = F,
-    upgrade = T
-)
-pak::pkg_install(
-    c('xtable', 'prettyunits', 'munsell', 'ggplot2', 'remotes'), dependencies = T, ask = F
-)
+    )
+pak::pkg_install(basePkg, dependencies = T, ask = F)
+pak::pkg_install(c('devtools', 'BiocVersion', 'BiocManager'), dependencies = T, ask = F, upgrade = T)
+pak::pkg_install(c('xtable', 'prettyunits', 'munsell', 'ggplot2', 'remotes'), dependencies = T, ask = F)
 remotes::install_github("SymbolixAU/googlePolylines", dependencies = T)
-
-install.packages(lib = '/usr/local/lib/R/site-library/',
-                 c('nleqslv', 'ade4', 'pixmap', 'stringi'),
-                 dependencies = T)
-
+install.packages(lib = '/usr/local/lib/R/site-library/', c('nleqslv', 'ade4', 'pixmap'), dependencies = T)
+install.packages(lib = '/usr/local/lib/R/site-library/', "stringi", dependencies = T)
