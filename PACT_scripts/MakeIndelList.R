4850c77103acb82e7eb6166b513b3bfe8599e5b7
jonserr <jnsrro@gmail.com>
2023-01-11 16:37:08 -0500

    Update MakeIndelList.R
    Updated function to take input argument

--- PACT_scripts/MakeIndelList.R
+++ PACT_scripts/MakeIndelList.R
@@ -1,6 +1,30 @@
+library("base")
+args <- commandArgs(TRUE)
+
+# Parameters Input trailing commandline -------------------------------------------------------
+args[1] -> pactRunName
+
+if(!require("stringr")){install.packages("stringr", dependencies = T)}
+if(!require("dplyr")){install.packages("dplyr", dependencies = T)}
 library("dplyr")
-inputFile <- sort(dir(path="~/Downloads", pattern="export_mytable",full.names=T),decreasing=T)[1]
-variantsData <- as.data.frame(read.csv(inputFile, strip.white = T))
+
+monthDay <- gsub("0(\\d)", "\\1", format(Sys.Date(), "%m_%d_"))
+thisYear <- format(Sys.Date(), "%Y.csv")
+
+thisFile <- paste0("export_mytable_", paste0(monthDay, thisYear))
+
+inputFile <- dir(path = file.path(fs::path_home(), "Downloads"), pattern = thisFile, full.names = T)
+
+if(length(inputFile)==0){
+    message("File not found: ", thisFile, "\n" ,"\nTrying other files in downloads folder:")
+    inputFile <- sort(dir(
+        path = file.path(fs::path_home(), "Downloads"), pattern = "export_mytable", full.names = T),
+        decreasing = T)[1]
+    message(inputFile)
+}
+
+variantsData <-as.data.frame(read.csv(inputFile, strip.white = T))
+
 varColumns <- c("Test_Number", "Gene.refGene", "IGV", "ExonicFunc.refGene")
 exonicFilter <- stringr::str_detect(variantsData$ExonicFunc.refGene, "frame|delet|insert")
 indelsList <- variantsData[exonicFilter, varColumns]
@@ -31,5 +55,8 @@ varsToCheck <- data.frame(
     "Comments" = '',
     "Variant" = indelsList$Variant
 )
-write.csv(varsToCheck, file="~/Desktop/Pact_desc.csv", quote=F, row.names=F)
-message("Output file saved: '~/Desktop/Pact_desc.csv'\n", "You can copy the contents of this file into your local PACT-22-##_desc.csv file as a template for consensus")
+
+outPutFile <- file.path(fs::path_home(), "Desktop", paste0(pactRunName, "_desc.csv"))
+
+write.csv(varsToCheck, file=outPutFile, quote=F, row.names=F)
+message("Output file saved: '", outPutFile, "'\n\n", "Copy this file into your consensus Rmd directory")
