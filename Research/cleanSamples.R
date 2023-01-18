317bb046912bed5d022d98a945c9faae12f1d310
jonserr <jnsrro@gmail.com>
2023-01-18 16:19:24 -0500

    Update cleanSamples.R

--- Research/cleanSamples.R
+++ Research/cleanSamples.R
@@ -10,20 +10,24 @@ writeTargets<- function(targets, csvFi="samplesheet.csv"){
   write.csv(targets, file=csvFi, quote=F, row.names=F)
 }
 
-setKeyColumns <- function(targets, col_samTypes, col_samNames, col_other) {
+CheckSamNames <- function(samNames, targets){
+  if(anyDuplicated(samNames) > 0){
+      warning("Sample IDs contain duplicates, assigning unique names")
+      samNames <- targets$Sample_ID = make.unique(samNames, sep="_")
+      targets$Sample_Name <- targets$Sample_ID <- samNames
+    }
+  return(targets)
+}
+
+SetKeyColumns <- function(targets, col_samTypes, col_samNames, col_other) {
     targets <- dfTargets(targets)
     targets$Type <- targets[, col_samTypes] # Creates any new "Type" column
     targets$Sample_Name <- targets$Sample_ID <- targets[, col_samNames] # generates Sample_ID column if doesn't exist
-    targets$Sample_Group <- targets[, col_other]
+    targets$Other_Group <- targets$Sample_Group <- targets[, col_other]
+
+    targets <- CheckSamNames(targets$Sample_ID, targets)
     return(targets)
   }
-
-checkSamNam <- function(samNames, targets){
-  if(anyDuplicated(samNames) > 0){
-  warning("Sample IDs contain duplicates, assigning unique names")
-  samNames <- targets$Sample_ID = make.unique(samNames, sep="_")
-  return(samNames) }else{return(samNames)}
-}
 
 getTargCsv <- function(csvFi = "samplesheet.csv") {
   gc(verbose = F)
