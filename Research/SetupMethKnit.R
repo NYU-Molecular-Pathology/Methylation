6dfd1dc98a08c80975ba438258f75d3cd8b4919e
jonserr <jnsrro@gmail.com>
2023-01-29 17:48:10 -0500

    Update SetupMethKnit.R

--- Research/SetupMethKnit.R
+++ Research/SetupMethKnit.R
@@ -1,10 +1,20 @@
 gb <- globalenv(); assign("gb", gb)
 # TODO: add args to load Differential + parrallel envir
-
+if(!require("devtools", warn.conflicts = F)){install.packages("devtools", dependencies=T)}
 # https://bookdown.org/yihui/rmarkdown-cookbook/custom-knit.html
 supM <- function(sobj){return(suppressMessages(suppressWarnings(sobj)))}
 supPk <- function(sobj){return(suppressPackageStartupMessages(sobj))}
 supSrt <- function(pk){return(suppressPackageStartupMessages(suppressWarnings(pk)))}
+
+if(!require("rprofile")){
+    devtools::install_github("csgillespie/rprofile", dependencies = T)
+}
+
+rprofile::set_startup_options(
+    show.signif.stars = FALSE,
+    useFancyQuotes = FALSE,
+    Ncpus = parallel::detectCores()-2
+)
 
 mainHub = "https://raw.githubusercontent.com/NYU-Molecular-Pathology/Methylation/main/Research/"
 rFiles = c(
@@ -19,33 +29,34 @@ rFiles = c(
     "pathviews.R",
     "pullRedcap_manual.R",
     "cleanSamples.R"
-#    "Differential.R"
+    #    "Differential.R"
 )
 
 pkgs <-
-  c(
-    "MEAL",
-    "SummarizedExperiment",
-    "S4Vectors",
-    "minfi",
-    "maxprobes",
-    "limma",
-    "IRanges",
-    "impute",
-    "IlluminaHumanMethylationEPICanno.ilm10b4.hg19",
-    "IlluminaHumanMethylation450kanno.ilmn12.hg19",
-    "GenomicRanges",
-    "conumee",
-    "BiocGenerics",
-    "Biobase",
-    "cowplot"
-  )
+    c(
+        "MEAL",
+        "SummarizedExperiment",
+        "S4Vectors",
+        "minfi",
+        "maxprobes",
+        "limma",
+        "IRanges",
+        "impute",
+        "IlluminaHumanMethylationEPICanno.ilm10b4.hg19",
+        "IlluminaHumanMethylation450kanno.ilmn12.hg19",
+        "GenomicRanges",
+        "conumee",
+        "BiocGenerics",
+        "Biobase",
+        "cowplot"
+    )
 
 scripts = paste0(mainHub, rFiles)
 suppressWarnings(lapply(scripts, function(i){message("Sourcing: ", i);devtools::source_url(i)}))
 supSrt(librarian::shelf(pkgs, ask = F, update_all = F, quiet = FALSE))
+
 extraDeps <- c("minfiData")
-supSrt(librarian::shelf(extraDeps, ask = F, update_all = F, quiet = FALSE))
+supSrt(librarian::shelf(extraDeps, ask = F, update_all = F, quiet = FALSE, dependencies = T))
 if(!require("maxprobes")){ devtools::install_github("markgene/maxprobes", dependencies = T)}
 if(!require("cnv.methyl")){devtools::install_github("https://github.com/ijcBIT/cnv.methyl.git", dependencies = T)}
 
@@ -70,15 +81,16 @@ library("magrittr")
 library("dplyr")
 
 setKnitDir <- function(runDir) {
-  syscmd <- paste("cd", runDir)
-  system(syscmd); setwd(runDir)
-  knitr::opts_knit$set(root.dir = runDir)
+    syscmd <- paste("cd", runDir)
+    system(syscmd); setwd(runDir)
+    knitr::opts_knit$set(root.dir = runDir)
 }
 
 CheckDirCreate <- function(pathLocation){
     dataOutDir <- file.path(getwd(), pathLocation)
     if (!dir.exists(dataOutDir)) {dir.create(dataOutDir)}
 }
+
 #Sys.setenv('R_MAX_VSIZE'=32000000000)
 set.seed(1234)
 
