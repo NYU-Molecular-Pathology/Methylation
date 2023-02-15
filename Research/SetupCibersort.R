e52e010e4480a3bc04b706550e24b6dc771af7e4
jonserr <jnsrro@gmail.com>
2023-02-15 16:20:39 -0500

    Create SetupCibersort.R

---
+++ Research/SetupCibersort.R
@@ -0,0 +1,109 @@
+#!/usr/bin/env R
+gb <- globalenv(); assign("gb", gb)
+
+InstPkg <- function(pkg, character.only=T){install.packages(pkg, dependencies=T, verbose=T, ask=F, quiet = F)}
+CheckReq <- function(pkgName){if(suppressWarnings(!require(pkgName, character.only = T, warn.conflicts = F))){InstPkg(pkgName)}}
+isMac <- ifelse(Sys.info()[['sysname']] == "Darwin", T, F)
+if(isMac==T){pkgPath <- "/Volumes/CBioinformatics/Methylation/Other_Packages"}
+
+gitLink <- "https://raw.githubusercontent.com/NYU-Molecular-Pathology/Methylation/main/Research/pullRedcap_manual.R"
+path_to_file <- file.path(pkgPath, "MethylCIBERSORT_0.2.0.tar.gz")
+path_to_ciber <- file.path(pkgPath, "CIBERSORT.R")
+# Installation ----------------------
+CheckReq("devtools")
+CheckReq("BiocManager")
+CheckReq("librarian")
+
+# Install all Required Packages  ------------------------------------------------------------------------------------------------
+package_list = c(
+    "BiocGenerics",
+    "IRanges",
+    "S4Vectors",
+    "ggpubr",
+    "rstatix",
+    "cellranger",
+    "styler",
+    "minfi",
+    "IlluminaHumanMethylation450kmanifest",
+    "IlluminaHumanMethylation450kanno.ilmn12.hg19",
+    "IlluminaHumanMethylationEPICmanifest",
+    "IlluminaHumanMethylationEPICanno.ilm10b4.hg19",
+    "limma",
+    "FlowSorted.Blood.450k",
+    "caret",
+    "glmnet",
+    "NMF",
+    "Rserve",
+    "e1071",
+    "parallel",
+    "preprocessCore",
+    "colorRamps",
+    "ggplot2",
+    "reshape2",
+    "ggpubr",
+    "readxl",
+    "stringr",
+    "tibble"
+)
+
+librarian::shelf("tidyr", ask=F, update_all = T, quiet = F)
+librarian::shelf(package_list, ask=F, update_all = T, quiet = F)
+devtools::source_url(gitLink)
+
+MergeSamSheets <- function(inputSheet, sampleSheet, colVariables){
+    ogExcel <- as.data.frame(readxl::read_xlsx(inputSheet))
+    if(any(duplicated(ogExcel))){
+        toDrop <- which(duplicated(ogExcel))
+        ogExcel <- ogExcel[-(toDrop),]
+        rownames(ogExcel) <- 1:nrow(ogExcel)
+    }
+    redCsv <- as.data.frame(read.csv(sampleSheet))
+    for (variableCol in colVariables) {
+        redCsv[, variableCol] <- ""
+    }
+
+    for (varCol in colVariables) {
+        for (sam in redCsv[,1]) {
+            currSamIdx <- which(ogExcel[,1]==sam)
+            data2add <- ogExcel[currSamIdx, varCol]
+            newSamIdx <- which(redCsv[,1]==sam)
+            redCsv[newSamIdx, varCol] <- data2add
+        }}
+    write.csv(redCsv, file = sampleSheet, quote = F, row.names = F)
+    return(redCsv)
+}
+
+
+# FUN: Save object as Rdata file
+SaveObj <- function(object, file.name){
+    outfile <- file(file.name, "wb")
+    serialize(object, outfile)
+    close(outfile)
+}
+
+# FUN: Load object from Rdata file with progressbar
+LoadRdatObj <- function(file.name, msgProg=T){
+    library("foreach")
+    library("utils")
+    filesize <- file.info(file.name)$size
+    chunksize <- ceiling(filesize / 100)
+    pb <- txtProgressBar(min = 0, max = 100, style=3)
+    infile <- file(file.name, "rb")
+    if(msgProg==T){
+        data <- foreach(it = icount(100), .combine = c) %do% {
+            setTxtProgressBar(pb, it)
+            readBin(infile, "raw", chunksize)
+        }
+    }else{
+        data <- foreach(it = icount(100), .combine = c) %do% {readBin(infile, "raw", chunksize)}
+    }
+    close(infile)
+    close(pb)
+    return(unserialize(data))
+}
+
+if(!require("MethylCIBERSORT")){install.packages(path_to_file, repos = NULL, type="source")}
+library("MethylCIBERSORT")
+require("stats")
+
+
