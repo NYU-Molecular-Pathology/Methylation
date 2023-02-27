021e3909e7da929dca3a2890c44e7b6cc3d02335
jonserr <jnsrro@gmail.com>
2023-02-27 12:40:44 -0500

    Update printPactCommands.sh

--- PACT_scripts/printPactCommands.sh
+++ PACT_scripts/printPactCommands.sh
@@ -358,7 +358,7 @@ msg_code "cd molecpathlab/development/NGS_QC_xf/"
 msg_code "conda install pip"
 msg_code "pip install -r requirements.txt"
 msg_step 5 "#d9d2e9" "Go to the QC python directory and generate the QC file"
-msg_code "conda activate && cd /gpfs/home/${kerbero}/molecpathlab/development/NGS_QC_xf/ && python molecpathlab/development/NGS_QC_xf/xf_pactqc.py -rdir ${molecDir}${runID}/output -pactid ${pactRun} && conda deactivate"
+msg_code "conda activate && cd /gpfs/home/${kerbero}/molecpathlab/development/NGS_QC_xf/ && python xf_pactqc.py -rdir ${molecDir}${runID}/output -pactid ${pactRun} && conda deactivate"
 echo "$BOX2"
 
 # Stage 4 -----------------------
