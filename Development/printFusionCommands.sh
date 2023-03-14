8c2523717b5dbdfcb9f8bfb8ee1e1970b1b57281
jonserr <jnsrro@gmail.com>
2023-03-14 11:03:43 -0400

    Update printFusionCommands.sh
    added python3

--- Development/printFusionCommands.sh
+++ Development/printFusionCommands.sh
@@ -3,6 +3,9 @@ FUSIONRUNID=${1-NULL} # if arg $1 is empty assign NULL as default else i.e. 1234
 FSID=${2-NULL}        # if arg $2 is empty assign NULL as default else i.e. PACT-21-28
 kerbero=${3-$USER}    # if arg $3 is empty assign $USER as default else i.e. whoami kerberosid
 
+python_version=$(python3 --version)
+python_path=$(which python3)
+
 FG_YLW='<span style="color:#cf6a00;">' # makes text darkorange color
 FG_RED='<span style="color:#cc0000">'  # makes text red color
 FG_GRN='<span style="color:#6aa84f">'  # makes text color green
@@ -265,6 +268,7 @@ echo "<h2 style='padding-top: 10px !important; -webkit-text-stroke-width: 1px; -
 msg_step 1 "white" "Sequencer Run ID FUSIONRUNID: ${FUSIONRUNID}</br>"
 msg_step 2 "white" "FUSION Run Name FSID: ${FSID}</br>"
 msg_step 3 "white" "Kerberos ID: ${kerbero}</br>"
+msg_step 4 "white" "Python3 path: ${python_version}</br>"
 echo "</br>"
 echo ' <div class="tocbox"> '
 #echo " "
@@ -326,7 +330,7 @@ msg_stage 3 "Generate HeatMap and QC"
 msg_step 1 "#baffc9" "After Xiaojun says the data is downloaded to the Z-drive, download the github repo for the QC"
 msg_code "gh repo clone NYU-Molecular-Pathology/FusionSeq_QC"
 msg_step 2 "#baffc9" "Execute the Python script in that directory using your sample inputs"
-msg_code "cd FusionSeq_QC && python3 control_QC_both.py -d '/Volumes/molecular/MOLECULAR LAB ONLY/NYU FUSION SEQer/${currYear} reports' -r ${FSID} -o $HOME/FusionSeq_QC"
+msg_code "cd FusionSeq_QC && ${python_path} control_QC_both.py -d '/Volumes/molecular/MOLECULAR LAB ONLY/NYU FUSION SEQer/${currYear} reports' -r ${FSID} -o $HOME/FusionSeq_QC"
 msg_step 3 "#baffc9" "After data is output, generate the QC and email"
 msg_code "Heatmaps are generated and are copied here:
 smb://shares-cifs.nyumc.org/apps/acc_pathology/molecular/MOLECULAR LAB ONLY/NYU FUSION SEQer/${currYear} reports/${FSID}/"
