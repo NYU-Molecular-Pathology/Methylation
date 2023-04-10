804b6bd84ce2edf6cb7f4366bec3ea2e6e372ac4
jonserr <jnsrro@gmail.com>
2023-04-10 16:06:44 -0400

    Update getRGsetBetas.R
    updated supbetaOutFi file path

--- Research/getRGsetBetas.R
+++ Research/getRGsetBetas.R
@@ -160,7 +160,7 @@ loadSupervise <- function(RGSet, betas, gb, superVar = NULL, dmpTyp = "categoric
     if (gb$supervisedRun == F) {
       return(NULL)
     }
-    supbetaOutFi <- paste(gb$supbetaOut, superVar , ".Rdata", sep = "_")
+    supbetaOutFi <- file.path(gb$runDir, paste(gb$supbetaOut, superVar , ".Rdata", sep = "_"))
     if (!file.exists(supbetaOutFi)) {
       rgRows <- RGSet@colData@rownames # ensure poor samples are dropped
       rgLiDat <- RGSet@colData@listData
