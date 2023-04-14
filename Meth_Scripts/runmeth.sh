da6029df65af4505af419352472d2474f7ba7d70
jonserr <jnsrro@gmail.com>
2023-04-14 19:37:54 -0400

    Update and rename runMeth.sh to runmeth.sh
    added comments

--- Meth_Scripts/runmeth.sh
+++ Meth_Scripts/runmeth.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+## ---------------------------
+## Script name: runmeth.sh
+## Purpose: cUrl source and execute Rscripts locally for clinical methylation report generation
+## Author: Jonathan Serrano
+## Copyright (c) NYULH Jonathan Serrano, 2023
+## ---------------------------
 
 # RedCap API Token: Saved Locally (Hardcoded)
 methAPI='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' 
 
-# INPUT ARGS: if args are empty assign NULL as default below
+# INPUT ARGS: if args are empty assign NULL as default below ---------------------------------------------------
 methRun=${1-NULL}  # methylation run id e.g. 22-MGDM17
 PRIORITY=${2-NULL} # string of prioritized RD-numbers
 runPath=${3-NULL}  # any custom directory to copy/run the idat files
@@ -15,7 +21,6 @@ GITHUBLINK="https://raw.githubusercontent.com/NYU-Molecular-Pathology/Methylatio
 GITHUBMAIN="https://raw.githubusercontent.com/NYU-Molecular-Pathology/Methylation/main/R/"
 
 # Hardcoded Terminal Color Variables ---------------------------------------------------------------------------
-# tput changes the console text output display
 BG_BLU="$(tput setab 4)" # makes text background blue
 BG_CYA="$(tput setab 6)" # makes text background cyan
 BG_MAG="$(tput setab 5)" # makes text background magenta
@@ -38,9 +43,9 @@ trap_error() {
 
 set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
 trap 'trap_error "${last_command}" $?' EXIT
-# CHECK: Prints instruction if methRun parameter is empty
+
+# CHECK: Prints instruction if methRun parameter is empty ------------------------------------------------------
 if [ -z "$methRun" ]; then
    echo "You did not provide a Methylation Run ID name. Ex. '21-MGDM30'"
    exit 1
@@ -50,6 +55,7 @@ if [ -z "$redcapUp" ]; then
    redcapUp="T"
 fi
 
+# SANITY CHECK: Prints parameters ------------------------------------------------------------------------------
 echo "~~~~~~~~~~~~~~~~~~~~~Parameters passed to RScript~~~~~~~~~~~~~~~~~~~~~"
 echo "${BG_BLU}methAPI:${normal} $methAPI"
 echo "${BG_CYA}(arg1) methRun:${normal} $methRun"
@@ -58,6 +64,7 @@ echo "${BG_GRN}(arg3) Custom runPath:${normal} $runPath"
 echo "${BG_BLU}(arg4) To upload redcapUp:${normal} $redcapUp"
 printf "\nCurl files downloaded:\n"
 
+# FUNCTIONS: Curl download and mkdir if not exists -------------------------------------------------------------
 message_curl() {
    linkname=$1
    filename=$2
@@ -80,8 +87,11 @@ message_curl ${GITHUBMAIN} "methylExpress.R"
 printf "\nExecuting Rscript with parameters input:\n"
 color_params="${BG_MAG}$methAPI${normal} ${BG_CYA}$methRun${normal} ${BG_GRN}$PRIORITY${normal} ${BG_BLU}$runPath${normal} ${BG_MAG}$redcapUp${normal} ${BG_GRN}$runLocal${normal}\n"
 printf "${BG_BLU}${bold}Rscript --verbose $HOME/methylExpress.R${normal} $color_params"
+
+# EXECUTE: Main command using user input  ----------------------------------------------------------------------
 Rscript --verbose $HOME/methylExpress.R $methAPI $methRun $PRIORITY $runPath $redcapUp $runLocal
 
+# ASSIGN: Default output paths ---------------------------------------------------------------------------------
 if [ -n "$runPath" ]; then
    DEFAULTPATH="/Volumes/CBioinformatics/Methylation/Clinical_Runs/${methRun}/"
    runPath=$DEFAULTPATH
@@ -91,11 +101,14 @@ LOCALDIR="$HOME/${methRun}"
 RUNYEAR=${methRun%%-*}
 MOLECDIR="/Volumes/molecular/MOLECULAR LAB ONLY/NYU-METHYLATION/Results/20${RUNYEAR}"
 
+# SANITY CHECK: Prints output directories ----------------------------------------------------------------------
 echo "${BG_GRN}runPath:${normal} $runPath"
 echo "${BG_GRN}MOLECDIR:${normal} $MOLECDIR"
 echo "${BG_GRN}LOCALDIR:${normal} $LOCALDIR"
 
+# EXECUTE: Rsync html to HOME directory then cp from HOME to Z-Drive -------------------------------------------
 check_directory "$LOCALDIR"
+
 printf "\n${BG_BLU}Rsyncing files:${normal}\n"
 rsync -vrthP --include '*.html' --exclude '*' "${runPath}" "$LOCALDIR"
 
