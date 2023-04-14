8873b7a15c3229e9f67df05e0a71e6945745934c
jonserr <jnsrro@gmail.com>
2023-04-14 11:20:49 -0400

    Add files via upload

---
+++ R/Report-Scripts/BRCA_Functions.R
@@ -0,0 +1,119 @@
+## Script name: BRCA_Functions.R
+## Purpose: source of global scripts imported for methylation report.Rmd BRCA1 and BRCA2 loci status
+## Author: Jonathan Serrano
+## Copyright (c) NYULH Jonathan Serrano, 2023
+## ---------------------------
+gb <- globalenv(); assign("gb", gb)
+
+
+GetBRCA_data <- function(ratioSet, RGset, brcaProbes){
+    #Output is DF with sample information, betas for relevant BRCA loci
+    brcaBetas = minfi::getBeta(ratioSet)[brcaProbes,]
+    brcaBetas <- as.data.frame(brcaBetas)
+    for (bal in 1:nrow(brcaBetas)) {
+        if (is.nan(brcaBetas[bal, 1])) {brcaBetas[bal, 1] = 0.00}
+        brcaBetas[bal, 1] <- round(brcaBetas[bal, 1], digits = 3)
+        if (brcaBetas[bal, 1] < 0.09) {brcaBetas[bal, 1] = 0.00}
+        if (brcaBetas[bal, 1] > 1.00) {brcaBetas[bal, 1] = 1.00}
+        else (brcaBetas[bal, 1] = brcaBetas[bal, 1])}
+    #Calculate detection of Pvalues using minfi method #c(MGMT_CGIDS,brcaProbes),]
+    detection.pvals = minfi::detectionP(RGset)
+    l.pvals = detection.pvals[c(brcaProbes),]
+    l.pvals <- as.data.frame(l.pvals)
+    for (lpv in 1:nrow(l.pvals)) {
+        if (is.nan(l.pvals[lpv,1])) {l.pvals[lpv,1] = 0.00}
+        l.pvals[lpv,1] <- round(l.pvals[lpv,1], digits = 3)
+        if (l.pvals[lpv,1] < 0.09){l.pvals[lpv,1] = 0.00}
+        if (l.pvals[lpv,1] > 1.00){l.pvals[lpv,1] = 1.00} else {l.pvals[lpv,1] = l.pvals[lpv,1]}
+    }
+    #calculate number of methylated loci with beta values above cutoff for mlh1
+    pos.loci = brcaBetas > 0.50
+    m.reslt = rep("", nrow(brcaBetas))
+    m.reslt[pos.loci == 5] = "POSITIVE"
+    m.reslt[pos.loci >= 0 & pos.loci < 4] = "NEGATIVE"
+    m.reslt[pos.loci == 4] = "INDETERMINATE"
+
+    clin.res = data.frame(
+        "Sample_Name" = 1,
+        "betaValues" = brcaBetas[,1],
+        "BRCA.pos.loci" = pos.loci[,1],
+        "m.reslt" = m.reslt,
+        "Pvalue" = l.pvals,
+        stringsAsFactors = F
+    )
+    return(clin.res)
+}
+
+renderBRCA_plot <- function(beta.matrix, clin.res, brca="BRCA1") {
+    clinVals <- as.data.frame(t(clin.res[2:5]))
+    col_vect <- c('#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe')
+    col_vect <- col_vect[1:nrow(clin.res)]
+    Probe.Names = paste(colnames(clinVals), brca, "probe", sep = ".")
+    names(Probe.Names) <- col_vect
+    df <-
+        data.frame(
+            x = as.numeric(paste(clinVals[1, ])),
+            y = seq(0.1,2, length.out=nrow(clin.res)),
+            Probe.Names = Probe.Names
+        )
+    density <- gb$sanitizeDense(beta.matrix)
+    fig <- ggplot(data = df, aes(x, y)) +
+        geom_bar(aes(fill = Probe.Names),stat = 'identity', width = 0.05, color="black") +
+        scale_fill_manual("legend", values = names(Probe.Names)) +
+        geom_line(density, mapping = aes(x = x, y = y), color="black") +
+        xlab('Beta Values') + ylab('Density') + theme(panel.background = element_blank()) +
+        guides(fill=guide_legend(title="BRCA Probes"))
+    return(suppressWarnings(fig))
+}
+
+getBRCA_table <- function(clin.res, brca) {
+    title.string = c(
+        "BRCA Methylation Status" = clin.res$m.reslt[1],
+        "Total Methylated Loci" = length(which(clin.res$BRCA.pos.loci))
+    )
+    title.string <- as.data.frame(title.string)
+    colnames(title.string) = NULL
+    return(title.string)
+}
+
+BRCAstatus <- function(RGset, brca="BRCA1") {
+    brca1Probes <- c(
+        "cg22435081",
+        "cg20130344",
+        "cg26081003",
+        "cg24900425",
+        "cg15109179",
+        "cg10527302",
+        "cg14780845",
+        "cg05368731",
+        "cg26370022",
+        "cg11529738"
+    )
+
+    brca2Probes <- c(
+        "cg09698981",
+        "cg19634711",
+        "cg12836863",
+        "cg05153735",
+        "cg26458617",
+        "cg24674189",
+        "cg15856230"
+    )
+    if(brca=="BRCA1") {
+        brcaProbes <- brca1Probes
+    } else{
+        brcaProbes <- brca2Probes
+    }
+    Mset = minfi::preprocessIllumina(RGset, bg.correct = TRUE, normalize = "controls")
+    ratioSet = ratioConvert(Mset, what = "both", keepCN = FALSE)
+    clin.res <- GetBRCA_data(ratioSet, RGset, brcaProbes)
+    par(mar = c(5, 6, 4, 1) + .1)
+    beta.matrix = getBeta(ratioSet)
+    brcaPlot1 <- renderBRCA_plot(beta.matrix, clin.res, brca)
+    mlhP <- getBRCA_table(clin.res, brca)
+    return(list(
+        "brcaPlot1" = brcaPlot1,
+        "mlhP" = gb$drawPlotTab(mlhP),
+        "theValue" = clin.res
+    ))
+}
