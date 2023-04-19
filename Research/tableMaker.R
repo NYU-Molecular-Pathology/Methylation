80e740c5621488bc5efcfc0ef26453e67cf601fd
jonserr <jnsrro@gmail.com>
2023-04-19 16:09:29 -0400

    Update tableMaker.R

--- Research/tableMaker.R
+++ Research/tableMaker.R
@@ -619,8 +619,8 @@ ShowKnitProgress <- function() {
   library("knitr")
   library("knitrProgressBar")
   library("progressr")
-  opts_knit$set(progress = TRUE, verbose = TRUE)
-  opts_hooks$set(
+  knitr::opts_knit$set(progress = TRUE, verbose = TRUE)
+  knitr::opts_hooks$set(
     log = function(options) {
       parsed <- parse(text = options$code, keep.source = FALSE)
       newCode <- sapply(as.character(parsed), function(x) {
@@ -632,6 +632,21 @@ ShowKnitProgress <- function() {
       return(options)
     }
   )
+  
+  knitr::knit_hooks$set(time_it = local({
+  now <- NULL
+  function(before, options) {
+    if (before) {
+      # record the current time before each chunk
+      now <<- Sys.time()
+    } else {
+      # calculate the time difference after a chunk
+      res <- difftime(Sys.time(), now)
+      # return a character string to show the time
+      paste("Time for this code chunk to run:", res)
+    }
+  }
+}))
   
   options(
     knitr.progress.fun = function(total, labels) {
