7be34229bd2863ebb3d9789db37690ebb6d01975
jonserr <jnsrro@gmail.com>
2023-04-25 11:26:18 -0400

    Update CalcTumorPurity.R

--- Development/CalcTumorPurity.R
+++ Development/CalcTumorPurity.R
@@ -1,52 +1,22 @@
-#Tumor purity refers to the proportion of cancer cells in a tumor sample relative to non-cancerous cells (e.g., immune cells, stromal cells). Estimating tumor purity is important for understanding tumor biology and improving the interpretation of molecular data from tumor samples. To calculate tumor purity from Illumina EPIC microarray .idat files in R, you can use the 'estimate' package. Here's a step-by-step guide:
+# Calculate tumor purity from Illumina EPIC microarray .idat files in R, use the 'estimate' package.
 
-#1. Install the necessary R packages, including the 'estimate' package for tumor purity estimation and the 'minfi' package for reading .idat files:
-
-```R
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-BiocManager::install("minfi")
-BiocManager::install("estimate")
-```
-
+# Install R packages, 'estimate' package for tumor purity estimation and the 'minfi' package for reading .idat files:
+if (!requireNamespace("BiocManager", quietly = T)){install.packages("BiocManager")}
+if (!requireNamespace("minfi", quietly = T)){BiocManager::install("minfi")}
+if (!requireNamespace("minfi", quietly = T)){BiocManager::install("estimate")}
 #2. Load the 'minfi' and 'estimate' packages:
-
-```R
-library(minfi)
-library(estimate)
-```
-
-#3. Read the .idat files using the 'minfi' package:
-
-```R
+library("minfi")
+library("estimate")
+# Read the .idat files using the 'minfi' package:
 base_path <- "path/to/your/idat/files"
 targets <- read.metharray.sheet(base_path)
 RGset <- read.metharray.exp(targets = targets)
-```
-
-#4. Preprocess the data and create a methylation M-value matrix:
-
-```R
+# Preprocess the data and create a methylation M-value matrix:
 Mset <- preprocessQuantile(RGset, normalize = "controls")
 Mval_matrix <- getM(Mset)
-```
-
-#5. Convert the M-value matrix to a beta-value matrix:
-
-```R
+# Convert the M-value matrix to a beta-value matrix:
 Beta_matrix <- minfi::m2beta(Mval_matrix)
-```
-
-#6. Estimate tumor purity using the 'estimate' package:
-
-```R
+# Estimate tumor purity using the 'estimate' package:
 purity_results <- estimate_purity(Beta_matrix, platform = "IlluminaHumanMethylationEPIC")
-```
-
-The 'purity_results' object will contain the tumor purity estimates for each sample in your dataset. You can extract the tumor purity values using:
-
-```R
+#The 'purity_results' object contains the tumor purity estimates for each sample. Extract the tumor purity values using:
 tumor_purity_values <- purity_results$Purity
-```
-
-By following these steps, you can calculate tumor purity from tumor DNA on Illumina EPIC microarray .idat files in R using the 'estimate' package.
