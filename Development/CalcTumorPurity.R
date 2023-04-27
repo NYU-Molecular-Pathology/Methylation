cc461edb6dd7edd946db5f6879fb770a88ec43e4
jonserr <jnsrro@gmail.com>
2023-04-27 11:27:56 -0400

    Update CalcTumorPurity.R

--- Development/CalcTumorPurity.R
+++ Development/CalcTumorPurity.R
@@ -1,22 +1,32 @@
-# Calculate tumor purity from Illumina EPIC microarray .idat files in R, use the 'estimate' package.
 
-# Install R packages, 'estimate' package for tumor purity estimation and the 'minfi' package for reading .idat files:
-if (!requireNamespace("BiocManager", quietly = T)){install.packages("BiocManager")}
-if (!requireNamespace("minfi", quietly = T)){BiocManager::install("minfi")}
-if (!requireNamespace("minfi", quietly = T)){BiocManager::install("estimate")}
-#2. Load the 'minfi' and 'estimate' packages:
-library("minfi")
-library("estimate")
-# Read the .idat files using the 'minfi' package:
-base_path <- "path/to/your/idat/files"
-targets <- read.metharray.sheet(base_path)
-RGset <- read.metharray.exp(targets = targets)
-# Preprocess the data and create a methylation M-value matrix:
-Mset <- preprocessQuantile(RGset, normalize = "controls")
-Mval_matrix <- getM(Mset)
-# Convert the M-value matrix to a beta-value matrix:
-Beta_matrix <- minfi::m2beta(Mval_matrix)
-# Estimate tumor purity using the 'estimate' package:
-purity_results <- estimate_purity(Beta_matrix, platform = "IlluminaHumanMethylationEPIC")
-#The 'purity_results' object contains the tumor purity estimates for each sample. Extract the tumor purity values using:
-tumor_purity_values <- purity_results$Purity
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager")
+}
+
+BiocManager::install("minfi")
+BiocManager::install("FlowSorted.Blood.EPIC")
+
+library(minfi)
+library(FlowSorted.Blood.EPIC)
+
+# Set the path to the folder containing the .idat files
+idat_path <- "path/to/your/idat/files"
+
+# Read .idat files using minfi package
+RGset <- minfi::read.metharray.exp(targets = minfi::getAnnotation(idat_path))
+
+# Preprocess the data using preprocessNoob from minfi package
+noob_data <- minfi::preprocessNoob(RGset)
+
+# Obtain beta values
+beta_values <- minfi::getBeta(noob_data)
+
+# Load the normal reference set
+data("FlowSorted.Blood.EPIC")
+normal_reference <- FlowSorted.Blood.EPIC
+
+# Estimate cell counts using the Houseman algorithm
+cell_counts <- minfi::estimateCellCounts(beta_values, reference = normal_reference)
+
+# Calculate tumor purity
+tumor_purity <- 1 - cell_counts[,"Bcell"]
