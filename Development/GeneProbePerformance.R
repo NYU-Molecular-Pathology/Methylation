b71298475268123e6c0f27ee19f5398f9ae9077b
jonserr <jnsrro@gmail.com>
2023-05-02 13:56:02 -0400

    Update GeneProbePerformance.R

--- Development/GeneProbePerformance.R
+++ Development/GeneProbePerformance.R
@@ -3,13 +3,31 @@
 ## Script name: GenePerformProbes.R
 ## Purpose: Select probes for island regions of an input gene and test probe performance
 ## Author: Jonathan Serrano
-## Copyright (c) NYULH Jonathan Serrano, 2023
+## Copyright (c) NYULH 2023
 ## ---------------------------
 
-if (!requireNamespace("BiocManager", quietly = TRUE)) {install.packages("BiocManager")}
-library(minfi)
-library(minfiDataEPIC)
-library(IlluminaHumanMethylationEPICanno.ilm10b4.hg19)
+# Load or Install required packages
+LibLoad <- function(pkg){
+    libOpt <- list(pkg, quietly = T, logical.return = T, warn.conflicts = F)
+    suppressPackageStartupMessages(suppressWarnings(do.call(library, libOpt)))
+}
+CheckReq <- function(pkg){
+    return(suppressWarnings(!requireNamespace(pkg, character.only = T, warn.conflicts = F)))
+}
+BioCinst <- function(pkg) {
+    if (CheckReq(pkg)) {do.call(BiocManager::install, list(pkg, update = F, ask = F))}
+    return(LibLoad(pkg))
+}
+CheckCran <- function(pkg) {
+    if(CheckReq(pkg)){do.call(install.packages, list(pkg, dependencies=T))}
+    return(LibLoad(pkg))
+}
+
+pkgLis <- c("utils", "grDevices", "stringr", "BiocManager")
+bioPkg <- c("minfi", "minfiDataEPIC", "IlluminaHumanMethylationEPICanno.ilm10b2.hg19")
+pkgLoad <- unlist(lapply(pkgLis, CheckCran))
+bioLoad <- unlist(lapply(bioPkg, BioCinst))
+stopifnot(all(pkgLoad) & all(bioLoad))
 
 # Function to plot cg probe performance for gene islands
 PlotIslandProbes <- function(rgset, geneName="BRCA1") {
@@ -17,17 +35,24 @@ PlotIslandProbes <- function(rgset, geneName="BRCA1") {
     beta_values <- getBeta(mset)
     anno <- getAnnotation(rgset)
     gene_probes <- anno[grepl(anno$UCSC_RefGene_Name, pattern=geneName),]
-    island_probes <- gene_probes[gene_probes$Relation_to_Island == "Island",]
+    island_probes <- as.data.frame(gene_probes[gene_probes$Relation_to_Island == "Island",])
+    shortName <- lapply(stringr::str_split(island_probes$UCSC_RefGene_Name, ";", simplify = F), unique)
+    island_probes$UCSC_RefGene_Name <- unlist(lapply(shortName, paste, collapse = ";"))
+    message(paste(geneName, "probes found:"))
+    message(paste0(capture.output(island_probes[,c("chr", "pos", "UCSC_RefGene_Name", "Relation_to_Island" )]), collapse="\n"))
     beta_subset <- beta_values[rownames(island_probes),]
-    plot_data <- as.data.frame(cbind(island_probes, t(beta_subset)))
-    scopeRange <- c((ncol(island_probes)+1):ncol(plot_data))
-    thePlot <- boxplot(
-        plot_data[, scopeRange], names = rownames(island_probes),
+    plot_data <- as.data.frame(t(beta_subset))
+    return(boxplot(
+        plot_data, names = rownames(island_probes), col = palette.colors(n=ncol(nrow(plot_data))),
         main = paste("CG Probe Performance on Illumina EPIC Methylation Arrays for", geneName, "Islands"),
         xlab = "Probe Names", ylab = "Beta Values", las = 2, cex.axis = 0.8
-        )
-    return(thePlot)
+    ))
 }
 
 rgset <- minfiDataEPIC::RGsetEPIC
-PlotIslandProbes(rgset, "BRCA1")
+myGenes <- c("BRCA1", "BRCA2", "ATM", "RAD51C")
+
+for (gn in myGenes) {
+    PlotIslandProbes(rgset, gn)
+}
+
