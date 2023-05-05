211c65157ac7ba5111a9930147ef85736cee0591
jonserr <jnsrro@gmail.com>
2023-05-05 11:03:38 -0400

    Update pullRedcap_manual.R

--- Research/pullRedcap_manual.R
+++ Research/pullRedcap_manual.R
@@ -36,7 +36,7 @@ flds = c("record_id","b_number","tm_number","accession_number","block","diagnosi
          "organ","tissue_comments","run_number", "nyu_mrn")
 
 # Load redcapAPI Package -----
-if(suppressWarnings(!requireNamespace(quietly=T, "redcapAPI"))){
+if(suppressWarnings(!require(quietly=T, "redcapAPI"))){
     params=list('nutterb/redcapAPI', dependencies=T, upgrade="always", type="source")
     do.call(devtools::install_github,c(params))
 }
@@ -69,14 +69,14 @@ loadPacks <- function(){
         bothType <- "source"
     }
     invisible(lapply(pkgs, function(pk){
-        if(suppressWarnings(!requireNamespace(quietly=T, pk, character.only=T))){
+        if(suppressWarnings(!require(quietly=T, pk, character.only=T))){
             install.packages(pk,dependencies=T, verbose=T, repos="http://cran.us.r-project.org", type=bothType)
         }}))
-    if(!requireNamespace(quietly=T, "redcapAPI")){install.packages("redcapAPI", dependencies = T, type=bothType,ask=F)}
-    if(!requireNamespace(quietly=T, "remotes")){install.packages("remotes", dependencies=T)}
+    if(!require(quietly=T, "redcapAPI")){install.packages("redcapAPI", dependencies = T, type=bothType,ask=F)}
+    if(!require(quietly=T, "remotes")){install.packages("remotes", dependencies=T)}
     library("redcapAPI")
     library(dplyr)
-    requireNamespace(quietly=T, 'foreach')
+    require(quietly=T, 'foreach')
 }
 
 # API Call functions -----
@@ -96,15 +96,131 @@ search.redcap <- function(rd_numbers, token=NULL, flds=NULL) {
     return(as.data.frame(result))
 }
 
+
+# Sets default variable paths/names
+getDefaults <- function() {
+    cbVol <- switch (Sys.info()[['sysname']],
+                     "Darwin" = "/Volumes/CBioinformatics/Methylation",
+                     "Linux" = "~/molecpathlab/production/Methylation"
+    )
+    moVol = "/Volumes/molecular"
+    rsVol = "/Volumes/snudem01labspace"
+    defaultParams <- data.frame(
+        mnp.pk.loc = paste0(file.path(cbVol, "classifiers/mnp.v11b6")),
+        ApiToken = "",
+        methDir = paste0(file.path(cbVol, "Clinical_Runs")),
+        clinDrv = paste0(file.path(moVol, "MOLECULAR LAB ONLY/NYU-METHYLATION")),
+        rschOut = paste0(file.path(rsVol, "FINAL_PDF_Reports_Brain")),
+        clinOut = paste0(file.path(moVol, "MOLECULAR/MethylationClassifier")),
+        rsch.idat = paste0(file.path(rsVol,"idats")),
+        clin.idat = paste0(file.path(moVol, "MOLECULAR/iScan")),
+        QC_file = paste0("~/Methyl_QC.Rmd"),
+        baseDir = paste0(file.path(cbVol, "Clinical_Runs")),
+        stringsAsFactors=F
+    )
+    return(defaultParams)
+}
+
+setVar <- function(valueName,val){
+    return(assign(valueName, val, envir=.GlobalEnv))
+}
+
+assignVar <- function(varStr, assignedVal){
+    return(
+        tryCatch(
+            expr = {if(!is.null(base::get(varStr))){cat("")}},
+            error = {setVar(varStr, assignedVal)},
+            finally = message(varStr," = ",assignedVal)
+        )
+    )
+}
+
+# returns assigned global variable values
+getSetvars <- function() {
+    assignedVars <- data.frame(
+        mnp.pk.loc = gb$mnp.pk.loc,
+        ApiToken = gb$ApiToken,
+        methDir = gb$methDir,
+        clinDrv = gb$clinDrv,
+        rschOut = gb$rschOut,
+        clinOut = gb$clinOut,
+        rsch.idat = gb$rsch.idat,
+        clin.idat = gb$clin.idat,
+        QC_file = gb$QC_file,
+        baseDir = gb$baseDir,
+        stringsAsFactors = F
+    )
+    return(assignedVars)
+}
+
+# Sets the default parameters for a methylation Run
+defineParams <- function(mnp.pk.loc = NULL,
+                         ApiToken = NULL,
+                         methDir = NULL,
+                         clinDrv = NULL,
+                         rschOut = NULL,
+                         clinOut = NULL,
+                         rsch.idat = NULL,
+                         clin.idat = NULL,
+                         QC_file = NULL,
+                         isMC = T,
+                         baseDir = NULL,
+                         runID = NULL,
+                         loadClassifier = T) {
+    defVars <- getDefaults()
+    inVars <- list(
+        mnp.pk.loc,
+        ApiToken,
+        methDir,
+        clinDrv,
+        rschOut,
+        clinOut,
+        rsch.idat,
+        clin.idat,
+        QC_file,
+        baseDir
+    )
+    i=1:length(inVars)
+    for (x in 1:length(defVars)) {
+        if(!is.null(inVars[[x]])){
+            defVars[1,x] <- inVars[[x]]
+        }
+    }
+    invisible(lapply(i,function(x){if(!is.null(inVars[[x]])){setVar(names(defVars[x]), inVars[[x]])}}))
+    i=1:length(defVars)
+    invisible(sapply(i,FUN=function(i){assignVar(names((defVars[i])), paste0(defVars[,i]))}))
+    cbVol <- switch (Sys.info()[['sysname']],
+                     "Darwin" = "/Volumes/CBioinformatics/",
+                     "Linux" = "~/molecpathlab/production/")
+    if (!isMC) {
+        methDir = rschOut
+        assign("workDir", cbVol)
+    }
+}
+
+
+
+setDirectory <- function(foldr) {
+    bsDir = paste("cd", foldr)
+    if (dir.exists(foldr)) {
+        system(bsDir)
+        setwd(foldr)
+        assign("workDir", foldr)
+    } else{
+        warning(paste("Location Not Found:", foldr))
+    }
+}
+
+
 # FUN: Sets your directory and sources the helper functions
 sourceFuns <- function(workingPath = NULL) {
     mainHub = "https://raw.githubusercontent.com/NYU-Molecular-Pathology/Methylation/main/"
-    script.list <- c("R/SetRunParams.R","R/CopyInputs.R","PACT_scripts/generateCNV.R")
+    script.list <- c("R/CopyInputs.R")
     if (is.null(workingPath)) {workingPath = getwd()}
     scripts <- paste0(mainHub, script.list)
     invisible(lapply(scripts, function(i){devtools::source_url(i)}))
-    gb$setDirectory(workingPath)
-    return(gb$defineParams(loadClassifier=F))
+    setDirectory(workingPath)
+    return(defineParams(loadClassifier=F))
 }
 
 readInfo <- function(inputSheet) {
@@ -119,9 +235,10 @@ readInfo <- function(inputSheet) {
         rds <- readxl::read_excel(inputSheet, sheet = 1)[, 1]
     }
     if(typeof(rds)!="character"){
-        warning('Converting RD-numbers to "Character" [1], readxl or read.delim should not output typeof=="list" update version of tibble')
+        warning('Converting RD-numbers to class "character", your readxl or read.delim package should not output typeof == "list". Update your version of the `tibble` package in the future!')
         rds <- as.data.frame(rds)[,1]
     }
+    rds <- rds[!is.na(rds)]
     return(rds)
 }
 
