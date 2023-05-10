e4c0204b3b61674884690d37fd8ce6f9948bcbc0
jonserr <jnsrro@gmail.com>
2023-05-10 15:15:08 -0400

    Update GeneProbePerformance.R

--- Development/GeneProbePerformance.R
+++ Development/GeneProbePerformance.R
@@ -280,3 +280,12 @@ myGenes <- c("BRCA1", "BRCA2", "ATM", "RAD51C")
 for (gn in myGenes) {
     PlotIslandProbes(beta_values, anno, gn)
 }
+row_not_all_na <- function(row) {
+  return(any(!is.na(row)))
+}
+
+# Apply the custom function to each row of the data.frame
+row_not_all_na_vec <- apply(df, 1, row_not_all_na)
+
+# Get the row names of rows where not all values are NA
+row_names_not_all_na <- row.names(df)[row_not_all_na_vec]
