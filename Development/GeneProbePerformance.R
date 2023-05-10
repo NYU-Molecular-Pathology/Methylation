f253c0fba10787fc6964fbf4e1767a5c58dc6943
jonserr <jnsrro@gmail.com>
2023-05-10 16:35:07 -0400

    Update GeneProbePerformance.R

--- Development/GeneProbePerformance.R
+++ Development/GeneProbePerformance.R
@@ -5,80 +5,69 @@
 ## Author: Jonathan Serrano
 ## Copyright (c) NYULH 2023
 ## ---------------------------
+
+# Global input variables
+myGenes <- c("BRCA1", "BRCA2", "ATM", "RAD51C")
 setwd("/Volumes/CBioinformatics/Methylation/Clinical_Runs/Probe_performance")
+
 # Load or Install required packages
-LibLoad <- function(pkg) {
-    libOpt <-
-        list(
-            pkg,
-            quietly = T,
-            logical.return = T,
-            warn.conflicts = F
-        )
+LibLoad <- function(pkg){
+    libOpt <- list(pkg, quietly = T, logical.return = T, warn.conflicts = F)
     suppressPackageStartupMessages(suppressWarnings(do.call(library, libOpt)))
 }
-CheckReq <- function(pkg) {
-    return(suppressWarnings(!require(
-        pkg,
-        character.only = T,
-        warn.conflicts = F
-    )))
+CheckReq <- function(pkg){
+    return(suppressWarnings(!require(pkg, character.only = T, warn.conflicts = F)))
 }
 BioCinst <- function(pkg) {
-    if (CheckReq(pkg)) {
-        do.call(BiocManager::install, list(pkg, update = F, ask = F))
-    }
+    if (CheckReq(pkg)) {do.call(BiocManager::install, list(pkg, update = F, ask = F))}
     return(LibLoad(pkg))
 }
 CheckCran <- function(pkg) {
-    if (CheckReq(pkg)) {
-        do.call(install.packages, list(pkg, dependencies = T))
-    }
+    if(CheckReq(pkg)){do.call(install.packages, list(pkg, dependencies=T))}
     return(LibLoad(pkg))
 }
 
-pkgLis <- c("utils", "grDevices", "stringr", "BiocManager", "ggplot2", "pals", "gridExtra")
+pkgLis <- c("utils", "grDevices", "stringr", "BiocManager", "ggplot2",
+            "pals", "gridExtra", "fitdistrplus", "ggh4x", "dplyr", "purrr")
 bioPkg <- c("minfi", "IlluminaHumanMethylationEPICanno.ilm10b4.hg19")
 pkgLoad <- unlist(lapply(pkgLis, CheckCran))
 bioLoad <- unlist(lapply(bioPkg, BioCinst))
 stopifnot(all(pkgLoad) & all(bioLoad))
 
+GrabSnpProbes <- function(anno){
+    snpCols <- c("Probe_rs" , "Probe_maf", "CpG_rs", "CpG_maf" , "SBE_rs" , "SBE_maf")
+    snpAnno <- as.data.frame(anno@listData[snpCols])
+    rownames(snpAnno) <- rownames(anno)
+    snpAnno <- snpAnno %>% filter_all(any_vars(!is.na(.)))
+    snpProbes <- rownames(snpAnno)
+    return(snpProbes)
+}
+
+
 GetIslandProbes <- function(anno, geneName, minProbes = 4) {
-    gene_probes <- anno[grepl(anno$UCSC_RefGene_Name, pattern = geneName), ]
-    islandRegions <- gene_probes$Relation_to_Island == "Island"
-    island_probes <- as.data.frame(gene_probes[islandRegions, ])
-    if(nrow(island_probes) < minProbes){
-        annoRegions <- gene_probes@listData[["GencodeCompV12_Group"]]!=""
-        island_probes <- as.data.frame(gene_probes[annoRegions, ])
-    }
-    shortName <-
-        lapply(lapply(
-            stringr::str_split(island_probes$UCSC_RefGene_Name, ";", simplify = F),
-            unique
-        ), sort)
-    toKeep <-
-        unlist(lapply(
-            shortName,
-            FUN = function(i) {
-                any(i == geneName)
-            }
-        ))
-    island_probes <- island_probes[toKeep, ]
-    shortName <- shortName[toKeep]
-    island_probes$UCSC_RefGene_Name <- unlist(lapply(shortName, paste, collapse = ";"))
+    gene_probes <- anno[grepl(anno$UCSC_RefGene_Name, pattern = geneName),]
+    snpProbes <- GrabSnpProbes(anno)
+    toKeep <- which(!rownames(gene_probes) %in% snpProbes)
+    gene_probes <- gene_probes[toKeep,]
+    #islandRegions <- gene_probes$Relation_to_Island == "Island"
+    island_probes <- gene_probes %>% as.data.frame() %>%
+        mutate(UCSC_RefGene_Name = str_split(UCSC_RefGene_Name, ";", simplify = F) %>%
+                   map( ~ sort(unique(.x)))) %>%
+        filter(map_lgl(UCSC_RefGene_Name, ~ any(.x == geneName))) %>%
+        mutate(UCSC_RefGene_Name = map_chr(UCSC_RefGene_Name, ~ paste(.x, collapse = ";")))
     message("")
-    island_probes <- island_probes %>%
-  mutate(UCSC_RefGene_Name = map(UCSC_RefGene_Name, ~ sort(unique(str_split(.x, ";"))))) %>%
-  filter(any(UCSC_RefGene_Name == geneName))
-shortName <- pull(island_probes, UCSC_RefGene_Name)
     message(paste(geneName, "probes found:"))
-    island_probes <-
-        island_probes[, c("chr", "pos", "UCSC_RefGene_Name", "Relation_to_Island")]
+    probeCols <- c("chr", "pos", "UCSC_RefGene_Name", "Relation_to_Island", "UCSC_RefGene_Group")
+    island_probes <- island_probes[, probeCols]
     csvFiName <- paste0(geneName, "_probes_used.csv")
     csvPath <- file.path(getwd(), "ProbesCSV")
-    if(!dir.exists(csvPath)){dir.create(csvPath)}
+    if (!dir.exists(csvPath)) {
+        dir.create(csvPath)
+    }
     message(paste0(capture.output(island_probes), collapse = "\n"))
-    write.csv(island_probes, file=file.path(csvPath, csvFiName), quote=F, row.names=T)
+    write.csv(
+        island_probes, file = file.path(csvPath, csvFiName), quote = F, row.names = T
+        )
     return(island_probes)
 }
 
@@ -131,61 +120,53 @@ GenerateProbePlot <- function(plot_data, plotCols, geneName) {
             fontface = "bold"
         ) +
         ggplot2::theme_bw() + txtTheme + labs(x = "Probe Names", y = "Beta Values") +
-        ggplot2::ggtitle(paste(mainTitle, geneName, "Islands"), )
+        ggplot2::ggtitle(paste(mainTitle, geneName, "Islands"),)
     return(thePlot)
 }
 
-plot_beta_values_distributions <-
-    function(beta_values_matrix,
-             specific_probes,
-             geneName) {
-        imgName <- paste(geneName, "probes_dist.png", sep = "_")
-        beta_vals_vec <- as.vector(beta_values_matrix)
-        probe_names <- rownames(beta_values_matrix)
-        methylation_data <- data.frame(Beta_Value = beta_vals_vec,
-                                       Probe_Name = probe_names)
-        methylation_data_subset <-
-            methylation_data[methylation_data$Probe_Name %in% specific_probes,]
-        pltTtl <-
-            paste("EPIC Methylation Beta Values Distributions for ",
-                  geneName,
-                  "Probes")
-        plot2 <- ggplot(methylation_data_subset, aes(x = Beta_Value)) +
-            geom_histogram(bins = 100,
-                           fill = "blue",
-                           alpha = 0.6) +
-            labs(title = pltTtl, x = "Beta Values", y = "Frequency") +
-            theme_minimal() + facet_wrap(~ Probe_Name, scales = "free_y") +
-            theme(
-                strip.text.x = element_text(size = 16),
-                panel.spacing = unit(.05, "lines"),
-                panel.border = element_rect(
-                    color = "black",
-                    fill = NA,
-                    linewidth = 1
-                ),
-                strip.background = element_rect(color = "black", linewidth = 1)
-            )
 
-        imgPath <- file.path(getwd(), "BetaDist")
-        if (!dir.exists(imgPath)) {
-            dir.create(imgPath)
-        }
-        message(paste0("\nSaving file:\n"), file.path(imgPath, imgName))
-        png(
-            file.path(imgPath, imgName),
-            width = 17,
-            height = 10,
-            units = "in",
-            res = 300
-        )
-        print(plot2)
-        invisible(dev.off())
+SaveDistPNG <- function(geneName, plot2) {
+    imgName <- paste(geneName, "probes_dist.png", sep = "_")
+    imgPath <- file.path(getwd(), "BetaDist")
+    if (!dir.exists(imgPath)) {
+        dir.create(imgPath)
     }
+    message(paste0("\nSaving file:\n"), file.path(imgPath, imgName))
+    png(
+        file.path(imgPath, imgName),
+        width = 17,
+        height = 10,
+        units = "in",
+        res = 300
+    )
+    print(plot2)
+    invisible(dev.off())
+}
 
 
+PlotBetaDist <- function(betasMat, island_probes, geneName) {
+    pltTtl <- paste("EPIC Methylation Beta Values Distributions for",
+              geneName, "Probes")
+    betaValSub <- data.frame(Beta_Value = as.vector(betasMat), Probe_Name = rownames(betasMat)) %>%
+        filter(Probe_Name %in% rownames(island_probes)) %>%
+        mutate(Probe_Name = paste(Probe_Name, island_probes[Probe_Name, "Relation_to_Island"],
+                                  island_probes[Probe_Name, "UCSC_RefGene_Group"], sep = "_"))
 
-SaveBoxPlot <- function(thePlot, ggTable, tbl1, tbl2 = NULL){
+    plot2 <- ggplot(betaValSub, aes(x = Beta_Value)) +
+        geom_histogram(bins = 50, fill = "blue", alpha = 0.6, aes(y = after_stat(density))) +
+        stat_theodensity() +
+        labs(title = pltTtl, x = "Beta Values", y = "Frequency") +
+        theme_minimal() + facet_wrap( ~ Probe_Name, scales = "free_y") +
+        theme(
+            strip.text.x = element_text(size = 10),
+            panel.spacing = unit(.05, "lines"),
+            panel.border = element_rect(color = "black", fill = NA,linewidth = 1),
+            strip.background = element_rect(color = "black", linewidth = 1)
+        )
+    SaveDistPNG(geneName, plot2)
+}
+
+SaveBoxPlot <- function(thePlot, ggTable, tbl1, tbl2 = NULL, geneName) {
     imgName <- paste(geneName, "probes_CNV.png", sep = "_")
     imgPath <- file.path(getwd(), "BetaBoxplots")
     if (!dir.exists(imgPath)) {
@@ -220,34 +201,17 @@ SaveBoxPlot <- function(thePlot, ggTable, tbl1, tbl2 = NULL){
     invisible(dev.off())
 }
 
-
-
-# Function to plot cg probe performance for gene islands
-PlotIslandProbes <- function(beta_values, anno, geneName = "BRCA1") {
-
-    island_probes <- GetIslandProbes(anno, geneName)
-    beta_subset <- beta_values[rownames(island_probes), ]
-    plot_data <- as.data.frame(t(beta_subset))
-    allCols <- paste(pals::polychrome(36))
-    allCols <- allCols[-2]
-    plotCols <-
-        if (ncol(plot_data) > 2) {
-            sample(allCols, ncol(plot_data))
-        } else{
-            c("darkred", "royalblue")
-        }
+GrabGGtable <- function(island_probes, plotCols) {
     ggTable <- as.data.frame(t(island_probes))
     mytheme <- gridExtra::ttheme_default(
         base_size = 10,
         colhead = list(bg_params = list(fill = plotCols, alpha = 0.8)),
         core = list(padding = unit(c(1.5, 1.5), "mm"))
     )
-
     if (ncol(ggTable) > 10) {
         ggTable1 <- ggTable[, 1:10]
         ggTable2 <- ggTable[, 10:ncol(ggTable)]
     }
-
     if (ncol(ggTable) <= 10) {
         tbl1 <- tableGrob(ggTable, theme = mytheme)
         tbl2 = NULL
@@ -262,46 +226,37 @@ PlotIslandProbes <- function(beta_values, anno, geneName = "BRCA1") {
         )
         tbl2 <- tableGrob(ggTable2, theme = mytheme2)
     }
+    return(list(ggTable=ggTable, tbl1=tbl1, tbl2=tbl2))
 
-    thePlot <- GenerateProbePlot(plot_data, plotCols, geneName)
-
-    SaveBoxPlot(thePlot, ggTable, tbl1, tbl2)
-
-    # Plot the beta values distributions for specific probes
-    specific_probes <- rownames(island_probes)
-    plot_beta_values_distributions(beta_values, specific_probes, geneName)
 }
+
+# Function to plot cg probe performance for gene islands
+PlotIslandProbes <- function(beta_values, anno, geneName) {
+
+    island_probes <- GetIslandProbes(anno, geneName)
+    beta_subset <- beta_values[rownames(island_probes),]
+    plot_data <- as.data.frame(t(beta_subset))
+    allCols <- paste(pals::polychrome(36))[-2]
+    plotCols <-  c("darkred", "royalblue")
+    if (ncol(plot_data) > 2) {
+        plotCols <- sample(allCols, ncol(plot_data), replace = T)
+    }
+    ggTab <- GrabGGtable(island_probes, plotCols)
+    #thePlot <- GenerateProbePlot(plot_data, plotCols, geneName)
+    #SaveBoxPlot(thePlot, ggTable=ggTab[["ggTable"]], tbl1=ggTab[["tbl1"]], tbl2=ggTab[["tbl2"]], geneName)
+    PlotBetaDist(beta_values, island_probes, geneName)
+}
+
 
 #rgset <- minfiDataEPIC::RGsetEPIC
 targets <- as.data.frame(read.csv(file.path(getwd(), "samplesheet.csv")))
+targets$Basename <- file.path(getwd(), targets$SentrixID_Pos)
 rgset <- minfi::read.metharray.exp(getwd(), targets, verbose = T, force = T)
+snpData <- minfi::getSnpInfo(rgset)
 mset <- minfi::preprocessRaw(rgset)
 beta_values <- minfi::getBeta(mset)
 anno <- minfi::getAnnotation(rgset)
 
-myGenes <- c("BRCA1", "BRCA2", "ATM", "RAD51C")
-
 for (gn in myGenes) {
     PlotIslandProbes(beta_values, anno, gn)
 }
-
-# Filter rows with any non-NA value
-filtered_df <- df %>% 
-  filter_all(any_vars(!is.na(.)))
-
-# Get the row names of rows where not all values are NA
-row_names_not_all_na <- row.names(filtered_df)
-
-# Print the row names
-print(row_names_not_all_na)
-
-island_probes <- island_probes %>%
-  rowwise() %>%
-  filter(any(sort(unique(str_split(UCSC_RefGene_Name, ";", simplify = F))) == geneName)) %>%
-  mutate(UCSC_RefGene_Name = paste(sort(unique(str_split(UCSC_RefGene_Name, ";", simplify = F))), collapse = ";"))
-  
-  island_probes <- island_probes %>%
-  mutate(UCSC_RefGene_Name = str_split(UCSC_RefGene_Name, ";", simplify = F) %>%
-           map(~ sort(unique(.x)))) %>%
-  filter(map_lgl(UCSC_RefGene_Name, ~ any(.x == geneName))) %>%
-  mutate(UCSC_RefGene_Name = map_chr(UCSC_RefGene_Name, ~ paste(.x, collapse = ";")))
