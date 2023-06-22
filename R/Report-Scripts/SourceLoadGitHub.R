d1da6d51f8dadaeeef14b3fc2c6384899cf128c0
jonserr <jnsrro@gmail.com>
2023-06-22 13:09:40 -0400

    Update SourceLoadGitHub.R
    added check for the report.rmd file downloaded locally to the home directory

--- R/Report-Scripts/SourceLoadGitHub.R
+++ R/Report-Scripts/SourceLoadGitHub.R
@@ -6,3 +6,9 @@ mainHub = "https://raw.githubusercontent.com/NYU-Molecular-Pathology/Methylation
 scriptList <- c("LoadInstallPackages.R", "SetRunParams.R", "MakeSampleSheet.R", "CopyInputs.R", "CopyOutput.R", "pipelineHelper.R", "CustomRuns.R")
 rmdScripts <- c("ClassTables.R", "MLH1_Functions.R", "PipeLineU.R", "RedcapOutput.R", "TsneFunctions.R", "cnvggplotly.R")
 LoadGitHubScripts(mainHub, scriptList); LoadGitHubScripts(file.path(mainHub,"Report-Scripts"), rmdScripts)
+gb$reportMd <- reportMd <- file.path(fs::path_home(),"report.Rmd")
+if(!file.exists(reportMd)){
+    if(!require("curl")){try(install.packages("curl", dependencies = T, ask = F), silent=T)}
+    url = "https://raw.githubusercontent.com/NYU-Molecular-Pathology/Methylation/main/R/report.Rmd"
+    curl::curl_download(url, reportMd, quiet = TRUE, mode = "wb", handle = curl::new_handle())
+}
