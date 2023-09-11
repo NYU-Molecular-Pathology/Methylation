f7d079c5ab3953875e15359127f9bb74aa4ce1e2
jonserr <jnsrro@gmail.com>
2023-09-11 13:54:26 -0400

    Update tableMaker.R
    updated ValidateSentrix to not be NULL

--- Research/tableMaker.R
+++ Research/tableMaker.R
@@ -321,8 +321,9 @@ ReadSheetType <- function(inputFi){
 
 
 ValidateSentrix <- function(targets, gb) {
-    if (is.null(gb$col_sentrix) | !any(gb$col_sentrix %in% colnames(targets))) {
+    if (is.null(gb$col_sentrix) | any(gb$col_sentrix %in% colnames(targets))) {
         message("Creating Sentrix ID column")
+        gb$col_sentrix <- "Sentrix_ID"
     }
     gb$GetCsvSheet(gb$needFi, gb$samsheet, gb$token, idatPath = gb$idatPath)
     targets <- gb$SetKeyColumns(
