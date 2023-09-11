bc786e8587f71f7ae5d2c2f260c789c42b16e4ef
jonserr <jnsrro@gmail.com>
2023-09-11 16:02:10 -0400

    Update Params_output.R

--- Research/params/Params_output.R
+++ Research/params/Params_output.R
@@ -1,15 +1,15 @@
 gb <- globalenv(); assign("gb", gb)
 # Default Output file names  -----------------------------------------------
 td <- format(Sys.Date(),"%b%d") # today's date Jun17
-rgOut <- paste0(td, "_RGset.Rdata")
-rawBetaFi <- paste0(td, "_betaRaw.Rdata")
-pValsOutFi <- paste0(td, "_DetPvals.Rdata")
-mbfile <- paste0(td, "_mSetSq.beta.Rdata")
-unbetaVariance <- paste0(td, "_unbetaVariance.RData")
-combatOut <- paste0(td, "_combatBetas.Rdata")
+rgOut <- file.path(gb$runDir, "data", paste0(td, "_RGset.Rdata"))
+rawBetaFi <- file.path(gb$runDir, "data", paste0(td, "_betaRaw.Rdata"))
+pValsOutFi <- file.path(gb$runDir, "data", paste0(td, "_DetPvals.Rdata"))
+mbfile <- file.path(gb$runDir, "data", paste0(td, "_mSetSq.beta.Rdata"))
+unbetaVariance <- file.path(gb$runDir, "data", paste0(td, "_unbetaVariance.RData"))
+combatOut <- file.path(gb$runDir, "data", paste0(td, "_combatBetas.Rdata"))
 
-supbetaOut <- paste0(td, "_supervisedBetas")
-annotFi <- paste0(td, "_annotations.rds")
+supbetaOut <- file.path(gb$runDir, "data", paste0(td, "_supervisedBetas"))
+annotFi <- file.path(gb$runDir, "data", paste0(td, "_annotations.rds"))
 
 # OutPut Directories
 gb$CheckDirCreate("data")
@@ -30,18 +30,18 @@ if(gb$genPairwise==T){
 }
 
 # Segments Copy Number Output Files
-segFile <- paste0(td, "_segfile.csv")
-seg_clust_file <- paste0(td, "_data_seg_clusters.txt")
-gsetFile <- paste0(td, "_gsetfunnorm.rds")
-gBetaFile <- paste0(td, "_gsetbeta.rds")
-ClusfiNam <- paste0(td, "_dmp.csv")
+segFile <- file.path(gb$runDir, "data", paste0(td, "_segfile.csv"))
+seg_clust_file <- file.path(gb$runDir, "data", paste0(td, "_data_seg_clusters.txt"))
+gsetFile <- file.path(gb$runDir, "data", paste0(td, "_gsetfunnorm.rds"))
+gBetaFile <- file.path(gb$runDir, "data", paste0(td, "_gsetbeta.rds"))
+ClusfiNam <- file.path(gb$runDir, "data", paste0(td, "_dmp.csv"))
 
 # Pathway Analysis Output Files
 #genesInputFi <- "yourFile.xlsx"
 #siteSpecific <- as.data.frame(readxl::read_excel(gb$genesInputFi))
 #your_genes <- siteSpecific$Gene
-geneListIn <- paste0(td,"_Genes_Pathway.csv")
-pathCsvOut <- paste0(td,"-signaling_pathway.csv")
+geneListIn <- file.path(gb$runDir, "data", paste0(td,"_Genes_Pathway.csv"))
+pathCsvOut <- file.path(gb$runDir, "data", paste0(td,"-signaling_pathway.csv"))
 
 # Plot Title Names  -----------------------------------------------
 clusType <- c("Unsupervised", "Supervised")
