e539777c02545e4e363f0c230cfdbaf2aff3b776
jonserr <jnsrro@gmail.com>
2023-09-22 12:45:05 -0400

    Update tableMaker.R

--- Research/tableMaker.R
+++ Research/tableMaker.R
@@ -521,7 +521,7 @@ MessageBatchMix <- function(targets, gb){
 
 PrintSamTypes <- function(targets, shCol) {
     if (!is.null(shCol)) {
-        samF <- setNames(as.data.frame(table(targets[, shCol]), NULL), c(paste("Type:", shCol), "----Total"))
+        samF <- setNames(as.data.frame(table(targets[, shCol]), NULL), c(paste("Type:", shCol), "---Total"))
         return(samF)
     }
 }
