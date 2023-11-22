f2371a2674a79f0a2fd523b6804f19c551afd2f5
jonserr <jnsrro@gmail.com>
2023-11-22 16:45:34 -0500

    Update pipelineHelper.R
    added color to progress message of samples that remain

--- R/pipelineHelper.R
+++ R/pipelineHelper.R
@@ -400,7 +400,7 @@ loopRender <- function(samList = NULL, data, redcapUp = T){
     
   currIdx = 1
   for (i in toRun) {
-      message(currIdx, " of ", length(toRun), " samples remaining to run")
+      message(bkGrn(dsh, currIdx, "of", length(toRun), "samples remaining to run", dsh))
       msgProgress(1, i, samList)
       do_report(data = data[i, ], gb$genCn)
       msgProgress(2, i, samList)
