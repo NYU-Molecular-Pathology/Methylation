f168e25df4a61d6d2e41b37d68cc0b358dfb75d4
jonserr <jnsrro@gmail.com>
2024-04-29 13:58:53 -0400

    Update SourceLoadGitHub.R
    added clear_sarc_dir function

--- R/Report-Scripts/SourceLoadGitHub.R
+++ R/Report-Scripts/SourceLoadGitHub.R
@@ -2,7 +2,7 @@
 ## ---------------------------
 ## Script name: SourceLoadGitHub.R
 ## Purpose: Function or importing scripts from Github for executing the Methylation Pipeline
-## Date Last Modified: January 19, 2024
+## Date Created: March 18, 2023
 ## Version: 1.0.0
 ## Author: Jonathan Serrano
 ## Copyright (c) NYULH Jonathan Serrano, 2024
@@ -23,6 +23,18 @@ rmdScripts <- c("ClassTables.R", "MLH1_Functions.R", "PipeLineU.R", "RedcapOutpu
 LoadGitHubScripts(mainHub, scriptList); LoadGitHubScripts(file.path(mainHub,"Report-Scripts"), rmdScripts)
 
 gb$reportMd <- reportMd <- file.path(fs::path_home(),"report.Rmd")
+
+clear_sarc_dir <- function(outputFi) {
+  if (file.exists(file.path(getwd(), outputFi))) {
+      file.remove(file.path(getwd(), outputFi))
+  }
+  
+  file.list <- dir(getwd(), pattern = ".html|.idat", full.names = T)
+  
+  if (length(file.list) > 0) {
+      file.remove(file.list)
+  }
+}
 
 if(!file.exists(reportMd)){
     if(!require("curl")){try(install.packages("curl", dependencies = T, ask = F), silent=T)}
