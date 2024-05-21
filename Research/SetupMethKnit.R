11039753426e8ffa89e827a2444d501dbcf1ee59
jonserr <jnsrro@gmail.com>
2024-05-21 16:31:17 -0400

    Update SetupMethKnit.R
    added install of cnv.methyl dependencies

--- Research/SetupMethKnit.R
+++ Research/SetupMethKnit.R
@@ -89,6 +89,7 @@ check_pkg_install("rmflight/knitrProgressBar")
 check_pkg_install("markgene/maxprobes")
 check_pkg_install("Ryo-N7/tvthemes")
 check_pkg_install("thomas-neitmann/mdthemes")
+librarian::shelf(c("fst", "itertools"), dependencies = T, ask = F)
 check_pkg_install("ijcBIT/cnv.methyl")
 
 if(Sys.info()[['sysname']]=="Linux") {
