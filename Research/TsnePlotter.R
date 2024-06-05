4aef713e7ac5cad59608db0327f85c7f93eff6c0
jonserr <jnsrro@gmail.com>
2024-06-05 16:02:56 -0400

    Update TsnePlotter.R
    updated LoopSupervisedPlots

--- Research/TsnePlotter.R
+++ Research/TsnePlotter.R
@@ -266,9 +266,11 @@ LoopSupervisedPlots <- function(targets, gb){
       targets1 <- gb$SubsetTargets(targets, gb$variable_to_filter)
       cat("\n\n")
       for(i in 1:length(gb$selectedVars)){
-          gb$superbetas <- eval(parse(text = paste0("gb$superbetas", i)))
-          gb$run_tsne_plots(targets1, gb$superbetas, gb,
-                            shapeVariable = gb$selectedVars[i], isSuper = T)
+          superVar <- gb$selectedVars[i]
+          supbetaOutFi <- paste0(gb$supbetaOut, "_", superVar, ".Rdata")
+          superbetas <- LoadRdatObj(supbetaOutFi)
+          gb$run_tsne_plots(targets1, superbetas, gb,
+                                         shapeVariable = gb$selectedVars[i], isSuper = T)
           cat("\n\n")
       }
   } else {cat("No supervised analysis output")}
