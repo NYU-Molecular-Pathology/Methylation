9ee0a8b06cb66716a2495b068deb0850364380b0
jonserr <jnsrro@gmail.com>
2024-06-10 16:46:09 -0400

    Update get_dependencies.R
    updated messaging

--- Meth_Scripts/get_dependencies.R
+++ Meth_Scripts/get_dependencies.R
@@ -39,6 +39,7 @@ require_pkg("utils", "binary")
 require_pkg("BiocManager", "binary")
 
 biocRepos <- suppressMessages(BiocManager::repositories())
+avail_bioc_packs <- suppressMessages(BiocManager::available())
 rbase_pkgs <- rownames(installed.packages(priority = "base"))
 pkg_info <- utils::available.packages(repos = biocRepos)
 
@@ -73,14 +74,54 @@ install_pkgs <- function(needed_pkgs, pkg_type = "source") {
     )
 }
 
+# FUN: Attempts source and binary install of Bioconductor ---------------------
+install_bio_pkg <- function(pkg_deps) {
+    params <- list(dependencies = c("Depends", "Imports", "LinkingTo"),
+                   ask = FALSE, update = TRUE)
+    for (pkg in pkg_deps) {
+        message("Installing BioCpackage dependency:")
+        tryCatch(
+            do.call(BiocManager::install, c(list(pkgs = pkg), params)),
+            error = function(e){
+                params$type <- "binary"
+                do.call(BiocManager::install, c(list(pkgs = pkg), params))
+            }
+        )
+    }
+}
+
+
+# FUN: Checks if package installed from BioConductor --------------------------
+check_bio_install <- function(pkgs) {
+    needed_pkgs <- check_needed(pkgs)
+
+    if (length(needed_pkgs) > 0) {
+        for (new_pkg in needed_pkgs) {
+            pkg_deps <- get_pkg_deps(new_pkg)
+            if (length(pkg_deps) > 0) {
+                install_bio_pkg(pkg_deps)
+            }
+            install_bio_pkg(new_pkg)
+        }
+    }
+    
+    sapply(pkgs, quiet_load)
+}
+
 
 # FUN: Installs any package dependencies and then the package -----------------
 try_install <- function(new_pkg) {
     message("Trying to install required package: ", new_pkg)
     pkg_deps <- get_pkg_deps(new_pkg)
-    if (length(pkg_deps) >= 1) {
+    if (length(pkg_deps) > 0) {
+          message("The following missing dependencies will be installed:\n", 
+            paste(pkg_deps, collapse = "\n"))
         for (pkg in pkg_deps) {
-            install_pkgs(pkg)
+            if (pkg %in% avail_bioc_packs){
+                check_bio_install(pkg)
+            } else {
+                install_pkgs(pkg)
+            }
         }
     }
     install_pkgs(new_pkg)
@@ -91,6 +132,8 @@ try_install <- function(new_pkg) {
 check_pkg_install <- function(pkgs) {
     pkg_deps <- check_needed(pkgs)
     if (length(pkg_deps) > 0) {
+        message("The following missing packages will be installed:\n", 
+            paste(pkg_deps, collapse = "\n"))
         for (new_pkg in pkg_deps) {
             try_install(new_pkg)
         }
