e74dae3638f848f9cdb79d84f6d870e869f7f46f
jonserr <jnsrro@gmail.com>
2024-06-10 16:17:13 -0400

    Update all_installer.R
    modified so R version required is greater than or equal

--- R/all_installer.R
+++ R/all_installer.R
@@ -14,7 +14,7 @@ options(Ncpus = 4)
 
 is_macos <- Sys.info()[['sysname']] == "Darwin"
 
-if (getRversion() < "4.2.2") {
+if (getRversion() <= "4.2.2") {
     stop("Your R version is ", R.version.string, ". Please update to R version 4.4.0 or later.")
 }
 
