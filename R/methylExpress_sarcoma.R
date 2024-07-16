104b29c4d79e4a755c5f64a3d295d3434e5e3082
jonserr <jnsrro@gmail.com>
2024-07-16 13:09:04 -0400

    Update methylExpress_sarcoma.R
    updated formatting and corrected header

--- R/methylExpress_sarcoma.R
+++ R/methylExpress_sarcoma.R
@@ -1,19 +1,22 @@
 #!/usr/bin/env Rscript
-## ---------------------------
-## Script name: pactParse.R
-## Purpose: source of global scripts and generate PACT -SampleSheet.csv file
-## Date Last Modified: January 12, 2024
+## Script name: methylExpress_sarcoma.R
+## Purpose: source global scripts to generate sarcoma reports for EPICv1 samples
+## Date Created: December 19, 2023
 ## Version: 1.0.0
 ## Author: Jonathan Serrano
-## Copyright (c) NYULH Jonathan Serrano, 2023
-## ---------------------------
+## Copyright (c) NYULH Jonathan Serrano, 2024
 
 gb <- globalenv(); assign("gb", gb)
-if (!require("devtools")) {
-    install.packages("devtools", dependencies = c("Depends", "Imports", "LinkingTo"), ask = FALSE)
+
+# Default Variables --------------------------------------------------
+gb$token <- gb$ApiToken <- token <- "XXXXXXXXXXXX_API_TOKEN_XXXXXXXXXX"
+selectRDs <- baseFolder <- NULL
+
+if (!requireNamespace("devtools", quietly = T)) {
+    install.packages("devtools", dependencies = T, ask = F, type = "binary")
 }
 
-args <- base::commandArgs(trailingOnly = TRUE)
+args <- base::commandArgs(trailingOnly = T)
 
 # Check if at least one argument is provided
 get_rd_args <- function(args) {
@@ -22,18 +25,18 @@ get_rd_args <- function(args) {
         stop("No RD-numbers provided!")
     } else {
         rd_numbers <- as.vector(args)
-        message("Input RD-number(s):\n",
-                paste0(capture.output(rd_numbers), collapse = "\n"))
+        message("RD-number(s) input:")
+        message(paste0(capture.output(rd_numbers), collapse = "\n"))
     }
     return(rd_numbers)
 }
 
 readFlag <- endsWith(args[1], ".csv")
 
-if(readFlag == FALSE){
+if (readFlag == FALSE) {
     rd_numbers <- get_rd_args(args)
     rd_csv <- NULL
-}else{
+} else {
     rd_csv <- args[1]
     rd_numbers <- NULL
 }
@@ -41,34 +44,32 @@ if(readFlag == FALSE){
 # rd_numbers = c("RD-23-1382")  # NULL # change to any individual numbers
 # rd_csv = NULL     # a list of RD-numbers to run in .csv column 1
 
-# Default Variables --------------------------------------------------
-gb$token <- gb$ApiToken <- token <- "XXXXXXXXXXXX_API_TOKEN_XXXXXXXXXX"
-selectRDs <- baseFolder <- NULL
-if(token == "XXXXXXXXXXXX_API_TOKEN_XXXXXXXXXX"){
-    stop("You must enter a valid REDCap API token for All Samples Database")
+if (token == "XXXXXXXXXXXX_API_TOKEN_XXXXXXXXXX") {
+    stop("A valid REDCap API token for 'All Samples Database' is required")
 }
+
 runID <- "MR23-rerun_sarc"
 getSarcHtml <- forcedUpload <- makeNewSheet <- runLocal <- T
 sarc_dir <- "/Volumes/CBioinformatics/Methylation/Clinical_Runs/Sarcoma_runs"
-cmd <- paste("cd", sarc_dir)
-system(cmd)
+system(paste("cd", sarc_dir))
 setwd(sarc_dir)
 outputFi = "samplesheet_sarc.csv"
-ghLink <- "https://raw.githubusercontent.com/NYU-Molecular-Pathology/Methylation/main/R"
+ghLink <-
+    "https://raw.githubusercontent.com/NYU-Molecular-Pathology/Methylation/main/R"
 
 # Source All Rscripts  -------------------------------------------------------
 devtools::source_url(file.path(ghLink, "Report-Scripts/SourceLoadGitHub.R"))
 
 if (makeNewSheet == T) {
-    if (file.exists(outputFi)) {file.remove(outputFi)}
+    if (file.exists(outputFi)) file.remove(outputFi)
     file.list <- dir(getwd(), pattern = ".html|.idat", full.names = T)
-    if (length(file.list) > 0) {file.remove(file.list)}
+    if (length(file.list) > 0) file.remove(file.list)
     gb$MakeLocalSampleSheet(runID, token, samSheetIn = rd_csv, rd_numbers, outputFi)
 }
 
 if (getSarcHtml == T) {
     gb$MakeSarcomaReport(targets = as.data.frame(read.csv(outputFi)))
-} else{
+} else {
     gb$StartCustomRun(redcapUp = F)
 }
 
