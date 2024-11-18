19e03ff694c9d80887874fc4a6d25e0a965cb4e7
jonserr <jnsrro@gmail.com>
2024-11-18 16:28:56 -0500

    Update BigPurpleCopy.sh
    updated to check if input is a file or a directory

--- PACT_scripts/BigPurpleCopy.sh
+++ PACT_scripts/BigPurpleCopy.sh
@@ -1,43 +1,53 @@
 #!/bin/bash
-## ---------------------------
 ## Script name: BigPurpleCopy.sh
 ## Purpose: Copy a file from a local directory to a BigPurple HPC directory
 ## Date Created: October 23, 2023
-## Date Last Modified: January 19, 2024
 ## Author: Jonathan Serrano
 ## Version: 1.0.0
 ## Copyright (c) NYULH Jonathan Serrano, 2024
-## ---------------------------
 
-INPUTFI_PATH=${1-NULL} # Full path to the file to be copied (i.e. /Users/myName/Desktop/myfile.csv)
-OUTDIR=${2-NULL}       # Full path to the directory on HPC where the file should be copied
-KERBEROS=${3-$USER}    # if arg $3 is empty assign USER as default
+INPUTFI_PATH=${1-NULL} # Full path to the file or directory to be copied
+OUTDIR=${2-NULL}       # Full path to the destination on HPC
 
-kerbero="$KERBEROS" # force console input as string
+kerbero="$USER"
 
-# Check if OUTDIR ends with a file separator slash ("/"). Append a slash if it doesn't.
+# Check if OUTDIR ends with a file separator ("/") & append if not
 if [[ "$OUTDIR" != */ ]]; then
-   OUTDIR="${OUTDIR}/"
+    OUTDIR="${OUTDIR}/"
 fi
 
-# Input Arguments ------------------------------------------------------------------------------------
+# Determine if INPUTFI_PATH is a file or directory
+if [[ -f "$INPUTFI_PATH" ]]; then
+    COPY_TYPE="file"
+elif [[ -d "$INPUTFI_PATH" ]]; then
+    COPY_TYPE="directory"
+else
+    echo "Error: The input '$INPUTFI_PATH' is not a file or directory."
+    exit 1
+fi
+
+# Input Arguments -------------------------------------------------------------
 FIBASE=$(basename "${INPUTFI_PATH}")
-HPC_FILE_DIR="${OUTDIR}${FIBASE}"
+if [[ "$COPY_TYPE" == "directory" ]]; then
+    HPC_FILE_DIR="${OUTDIR}${FIBASE}"  # Keep folder structure for directories
+else
+    HPC_FILE_DIR="${OUTDIR}"          # Copy file directly into the destination
+fi
 NEW_FILE_PATH="$kerbero@bigpurple.nyumc.org:$HPC_FILE_DIR"
 
-# Text Color Vars ------------------------------------------------------------------------------------
+# Text Color Vars -------------------------------------------------------------
 bold=$(tput bold)         # makes console output text bold
 BG_BLUE="$(tput setab 4)" # makes console output text background blue
 normal=$(tput sgr0)       # resets default console output text
 
-# FUN: Message a step in color blue ------------------------------------------------------------------
+# FUN: Message a step in color blue -------------------------------------------
 message_print() {
-   stringMsg=$1
-   extraMsg=$2
-   printf "\n${bold}${BG_BLUE}${stringMsg}:${normal}\n${extraMsg}\n\n"
+    stringMsg=$1
+    extraMsg=$2
+    echo -e "\n${bold}${BG_BLUE}${stringMsg}:${normal}\n${extraMsg}\n\n"
 }
 
-# FUN: Checks if BigPurple directory exists ----------------------------------------------------------
+# FUN: Checks if BigPurple directory exists -----------------------------------
 echo_hpc_command() {
     OUTDIR=$1
     kerbero=$2
@@ -55,13 +65,13 @@ check_directory() {
     fi
 }
 
-# Check if BigPurple Directory Directory Exists ----------------------------------------------
+# Check if BigPurple Directory Exists -----------------------------------------
 check_directory "$kerbero" "$OUTDIR"
 
-# Rsync file from local directory to BigPurple ------------------------------------------------------
+# Rsync file or directory from local to BigPurple -----------------------------
 message_print "Executing the following" "rsync -vrthP -e ssh $INPUTFI_PATH $NEW_FILE_PATH"
 rsync -vrthP -e ssh "$INPUTFI_PATH" "$NEW_FILE_PATH"
 
-# Change permissions of the file on BigPurple ------------------------------------------------------
+# Change permissions of the file or directory on BigPurple --------------------
 message_print "Changing permissions" "ssh $kerbero@bigpurple.nyumc.org chmod -R g+rwx $HPC_FILE_DIR"
 ssh "$kerbero@bigpurple.nyumc.org" "chmod -R g+rwx $HPC_FILE_DIR"
