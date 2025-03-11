2f71dea770863e58700fabbd52c58a4d1de4a3a1
jonserr <jnsrro@gmail.com>
2025-03-11 14:45:50 -0400

    Update SarcTables.R
    updated header and spacing in kable arguments for clarity

--- R/Report-Scripts/SarcTables.R
+++ R/Report-Scripts/SarcTables.R
@@ -1,34 +1,32 @@
 #!/usr/bin/env Rscript
-## ---------------------------
 ## Script name: SarcTables.R
 ## Purpose: Source Global scripts for Sarcoma analysis report tables
 ## Date Created: January 19, 2024
 ## Version: 1.0.0
 ## Author: Jonathan Serrano
-## Copyright (c) NYULH Jonathan Serrano, 2024
-## ---------------------------
+## Copyright (c) NYULH Jonathan Serrano, 2025
 
-library("base")
-gb <- globalenv(); assign("gb", gb)
+gb <- globalenv()
+assign("gb", gb)
 options(width = 320, scipen = 5, knitr.kable.NA = '')
 
 PredTable <- function(predLi){
     kgb <- c("striped", font_size = 9, bootstrap_options = c("bordered"))
     pt <- predLi$pred %>% 
-        knitr::kable("html",c(booktabs = T, escape = F, linesep = ""),align='clc') %>%
-        kableExtra::kable_styling(kgb, full_width = F, position="left") %>%
+        knitr::kable("html",c(booktabs = T, escape = F, linesep = ""), align = 'clc') %>% 
+        kableExtra::kable_styling(kgb, full_width = F, position = "left") %>% 
         kableExtra::row_spec(row = 0, font_size = 12, background = "rgb(220, 208, 255)", color = "black") %>%
         kableExtra::row_spec(row = 1, font_size = 11)
     return(pt)
 }
 
+
 PredTopTable <- function(predLi){
     kgb <- c("striped", font_size = 9, bootstrap_options = c("bordered"))
     pt <- predLi$predTop %>% 
-        knitr::kable("html",c(booktabs = T, escape = F, linesep = ""),align='clc') %>%
-        kableExtra::kable_styling(kgb, full_width = F, position="left") %>%
+        knitr::kable("html",c(booktabs = T, escape = F, linesep = ""), align = 'clc') %>% 
+        kableExtra::kable_styling(kgb, full_width = F, position = "left") %>% 
         kableExtra::row_spec(row = 0, font_size = 12, background = "rgb(211, 211, 211)", color = "black") %>%
         kableExtra::row_spec(row = c(1:3), font_size = 11)
     return(pt)
 }
-
