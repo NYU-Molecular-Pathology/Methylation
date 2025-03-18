fa9c1208633661a23a0265081715f19152dbc36b
jonserr <jnsrro@gmail.com>
2025-03-18 12:12:16 -0400

    Update CustomCNVanno.R
    refactored and renamed main function to annotate_cnv_with_genes to have less inputs and into separate functions

--- Research/CustomCNVanno.R
+++ Research/CustomCNVanno.R
@@ -5,9 +5,10 @@
 ## Author: Jonathan Serrano
 ## Copyright (c) NYULH Jonathan Serrano, 2025
 
-gb <- globalenv()
-
+# Example usage:
 my_idat <- "/path/to/file.idat"
+geneNames <- c("BRCA1", "FOXA1", "TP53")
+
 manifest_path = "/Volumes/CBioinformatics/Methylation/Manifests"
 ref_path <- "/Volumes/CBioinformatics/Methylation/Manifests/ref_v2_hg38.rds"
 pkgs <- c(
@@ -15,6 +16,7 @@ pkgs <- c(
     "conumee2",
     "sesame",
     "minfi",
+    "S4Vectors",
     "GenomicRanges",
     "IRanges",
     "dplyr",
@@ -39,19 +41,10 @@ if ( !not_installed("conumee2.0")) {
 
 missing_pkgs <- sapply(pkgs, not_installed)
 
-if (not_installed("pak")) {
-    install.packages("pak", ask = F, dependencies = T)
-}
-
-
-if (any(missing_pkgs)) {
-    needs_install <- pkgs[missing_pkgs]
-    pak::pkg_install(needs_install)
-}
-
+if (not_installed("pak")) install.packages("pak", ask = F, dependencies = T)
+if (any(missing_pkgs)) pak::pkg_install(pkgs[missing_pkgs], ask = FALSE)
 stopifnot(all(sapply(pkgs, requireNamespace, quietly = FALSE)))
 
-sesameData::sesameDataCacheAll()
 
 # Helper function: detect array type and genome build from an RGset.
 detect_array_info <- function(RGset) {
@@ -68,7 +61,6 @@ detect_array_info <- function(RGset) {
 }
 
 
-
 liftOver_Mset_manifest <- function(mset, v2Man) {
     mapping_vec <- setNames(v2Man$EPICv1_Loci, v2Man$IlmnID)
 
@@ -83,7 +75,6 @@ liftOver_Mset_manifest <- function(mset, v2Man) {
 
     return(mset)
 }
-
 
 
 # Helper function: retrieve gene ranges (e.g., TSS/promoter region) for a given gene symbol.
@@ -107,7 +98,7 @@ get_gene_ranges <- function(geneSymbols) {
         symbol = geneInfo$hgnc_symbol
     )
     gr <- GenomeInfoDb::keepStandardChromosomes(gr, pruning.mode = "coarse")
-    mcols(gr)$name <- mcols(gr)$symbol
+    S4Vectors::mcols(gr)$name <- S4Vectors::mcols(gr)$symbol
     return(gr)
 }
 
@@ -120,15 +111,16 @@ count_gene_probes <- function(gene_gr, probe_gr) {
 }
 
 
-
 alignCNVObjects <- function(query_obj, ref_obj, anno_obj) {
 
     qProbs <- rownames(query_obj@intensity)
-    query_int_df <- as.data.frame(do.call(cbind, lapply(query_obj@intensity, function(x) data.frame(x))))
+    query_int_df <- as.data.frame(
+        do.call(cbind, lapply(query_obj@intensity, function(x) data.frame(x))))
     rownames(query_int_df) <- qProbs
 
     rProbs <- rownames(ref_obj@intensity)
-    ref_int_df   <- as.data.frame(do.call(cbind, lapply(ref_obj@intensity, function(x) data.frame(x))))
+    ref_int_df   <- as.data.frame(
+        do.call(cbind, lapply(ref_obj@intensity, function(x) data.frame(x))))
     rownames(ref_int_df) <- rProbs
 
     query_obj@intensity <- query_int_df
@@ -146,8 +138,10 @@ alignCNVObjects <- function(query_obj, ref_obj, anno_obj) {
     query_probes <- rownames(query_int)
     ref_probes   <- rownames(ref_int)
     anno_probes <- anno_obj@probes@elementMetadata@listData[["IlmnID"]]
-    names(anno_obj@probes) <- anno_obj@probes@elementMetadata@listData[["IlmnID"]]
-    common_probes <- base::Reduce(intersect, list(query_probes, ref_probes, anno_probes))
+    names(anno_obj@probes) <-
+        anno_obj@probes@elementMetadata@listData[["IlmnID"]]
+    common_probes <- base::Reduce(intersect,
+                                  list(query_probes, ref_probes, anno_probes))
     if (length(common_probes) == 0)
         stop("No common probes found among query, reference, and annotation.")
     query_obj@intensity <- query_obj@intensity[common_probes, , drop = FALSE]
@@ -157,7 +151,7 @@ alignCNVObjects <- function(query_obj, ref_obj, anno_obj) {
 }
 
 
-modify_markers <- function(p_interactive) {
+modify_markers <- function(p_interactive, gene_gr) {
     gene_order <- p_interactive[["x"]][["data"]][[5]][["text"]]
     is_found <- gene_order %in% gene_gr$symbol
     orig_m <- p_interactive[["x"]][["data"]][[5]][["marker"]][["size"]]
@@ -173,13 +167,128 @@ modify_markers <- function(p_interactive) {
 }
 
 
+process_gene_ranges <- function(gene_gr, anno, min_probes = 4) {
+    if (is.null(gene_gr)) {
+        warning("No gene ranges could be retrieved for the supplied gene names.")
+        return("No gene ranges found for input gene list.")
+    }
+
+    probe_gr <- anno@probes
+    gene_probe_counts <- sapply(split(gene_gr, gene_gr$symbol), count_gene_probes, probe_gr = probe_gr)
+
+    gene_anno_msg <- ""
+    for (gene in names(gene_probe_counts)) {
+        nprobes <- gene_probe_counts[gene]
+        if (nprobes == 0) {
+            gene_anno_msg <- paste0(gene_anno_msg,
+                                    "No matches found for gene ",
+                                    gene,
+                                    " on the methylation array. ")
+        } else if (nprobes < min_probes) {
+            gene_anno_msg <- paste0(gene_anno_msg,
+                                    "Caution: only ",
+                                    nprobes,
+                                    " probes annotated for gene ",
+                                    gene,
+                                    ". ")
+        }
+    }
+
+    return(gene_anno_msg)
+}
+
+
+get_cnv_obj <- function(align_obj) {
+    cnv_obj <- conumee2::CNV.fit(query = align_obj$query,
+                                 ref = align_obj$ref,
+                                 anno = align_obj$anno)
+    cnv_obj <- conumee2::CNV.bin(cnv_obj)
+    cnv_obj <- conumee2::CNV.detail(cnv_obj)
+    cnv_obj <- conumee2::CNV.segment(cnv_obj)
+    return(cnv_obj)
+}
+
+
+get_final_plot <- function(cnv_obj, geneNames, gene_anno_msg, gene_gr) {
+    p_interactive <- conumee2::CNV.plotly(cnv_obj)
+    if (!is.null(gene_gr)) {
+        p_interactive <- modify_markers(p_interactive, gene_gr)
+    }
+
+    if (length(geneNames) <= 5) {
+        final_title <- paste("CNV Genome Plot With Genes –",
+                             paste(geneNames, collapse = ", "))
+    } else {
+        final_title <- "CNV Genome Plot With Custom Gene Annotations"
+    }
+
+    if (nchar(gene_anno_msg) > 0) {
+        final_title <- paste0(final_title, "\nNote: ", gene_anno_msg)
+    }
+    p_interactive <- plotly::layout(p_interactive, title = list(text = final_title))
+    return(p_interactive)
+}
+
+
+get_reference <- function(array_info) {
+    if (file.exists(ref_path)) {
+        ref <- readRDS(ref_path)
+        return(ref)
+    }
+
+    if (array_info$array == "EPICv2") {
+        message("Attempting to load EPICv2 reference from sesameData.")
+        available_refs <- sesameData::sesameDataList()
+        if ("EPICv2.8.SigDF" %in% available_refs$Title) {
+            ref_list <- sesameData::sesameDataGet("EPICv2.8.SigDF")
+            ref_mat <- do.call(cbind, lapply(ref_list, sesame::totalIntensities))
+            ref_df <- as.data.frame(ref_mat, check.names = FALSE)
+            ref <- conumee2::CNV.load(ref_df)
+            return(ref)
+        } else {
+            stop("EPICv2.8.SigDF not available in sesameData::sesameDataList()")
+        }
+    }
+
+    message("Using default reference from minfiData")
+    dt_pkgs <- c("minfiDataEPIC", "minfiData")
+    missing_pkgs <- sapply(pkgs, not_installed)
+    if (any(missing_pkgs)) pak::pkg_install(pkgs[missing_pkgs], ask = FALSE)
+
+    if (array_info$array == "EPIC") {
+        library("minfiDataEPIC")
+        data("MsetEPIC", package = "minfiDataEPIC", envir = environment())
+        ref <- conumee2::CNV.load(get("MsetEPIC", envir = environment()))
+    } else{
+        library("minfiData")
+        data("MsetEx", package = "minfiData", envir = environment())
+        ref <- conumee2::CNV.load(get("MsetEx", envir = environment()))
+    }
+
+    return(ref)
+}
+
+
+
 # Main function: process RGset, perform CNV analysis, annotate requested genes,
 # and generate an interactive genome plot with gene annotation.
-custom_cnv_plot_with_gene_annotation <- function(RGset, geneNames,
-                                                 ref = NULL, minProbeThreshold = 4) {
+annotate_cnv_with_genes <- function(my_idat, geneNames,
+                                                 ref = NULL, min_probes = 4) {
+
+    RGset <- minfi::read.metharray(my_idat, force = TRUE)
+
+    if (RGset@annotation[["array"]] == "IlluminaHumanMethylationEPICv2") {
+        RGset@annotation[["annotation"]] <- "20a1.hg38"
+        RGset@annotation[["Genome"]] <- "hg38"
+    } else{
+        RGset@annotation[["Genome"]] <- "hg19"
+    }
+
     array_info <- detect_array_info(RGset)
     message("Detected array type: ", array_info$array,
             " and genome build: ",array_info$genome)
+
+    gene_gr <- get_gene_ranges(geneNames)
 
     library("conumee2")
     Mset <- minfi::preprocessIllumina(RGset, normalize = "controls")
@@ -188,12 +297,7 @@ custom_cnv_plot_with_gene_annotation <- function(RGset, geneNames,
     con_path <- file.path(path.package("conumee2"), "data")
     load(file.path(con_path, "exclude_regions.rda"))
 
-    gene_gr <- get_gene_ranges(geneNames)
-
     anno <- conumee2::CNV.create_anno(
-        bin_minprobes = 15,
-        bin_minsize = 50000,
-        bin_maxsize = 5e6,
         array_type = array_info$array,
         chrXY = TRUE,
         genome = array_info$genome,
@@ -201,60 +305,22 @@ custom_cnv_plot_with_gene_annotation <- function(RGset, geneNames,
         detail_regions = gene_gr
     )
 
-    ref <- base::readRDS(ref_path)
+    if (is.null(ref)) {
+        ref <- get_reference(array_info)
+    }
 
     aligned <- alignCNVObjects(query_cnv, ref, anno)
     colnames(aligned$ref@intensity) <- colnames(ref@intensity)
 
-    cnv_obj <- conumee2::CNV.fit(query = aligned$query, ref = aligned$ref, anno = aligned$anno)
-    cnv_obj <- conumee2::CNV.bin(cnv_obj)
-    cnv_obj <- conumee2::CNV.detail(cnv_obj)
-    cnv_obj <- conumee2::CNV.segment(cnv_obj)
+    cnv_obj <- get_cnv_obj(aligned)
 
-    if (is.null(gene_gr)) {
-        warning("No gene ranges could be retrieved for the supplied gene names.")
-        gene_annotation_message <- "No gene ranges found for input gene list."
-    } else {
-        probe_gr <- anno@probes
-        gene_probe_counts <- sapply(split(gene_gr, gene_gr$symbol), count_gene_probes, probe_gr = probe_gr)
+    gene_anno_msg <- process_gene_ranges(gene_gr, anno, min_probes)
+    p_interactive <- get_final_plot(cnv_obj, geneNames, gene_anno_msg, gene_gr)
 
-        gene_annotation_message <- ""
-        for (gene in names(gene_probe_counts)) {
-            nprobes <- gene_probe_counts[gene]
-            if (nprobes == 0) {
-                gene_annotation_message <- paste0(gene_annotation_message,
-                                                  "No matches found for gene ", gene, " on the methylation array. ")
-            } else if (nprobes < minProbeThreshold) {
-                gene_annotation_message <- paste0(gene_annotation_message,
-                                                  "Caution: only ", nprobes, " probes annotated for gene ", gene, ". ")
-            }
-        }
-    }
-
-    p_interactive <- conumee2::CNV.plotly(cnv_obj)
-
-    if (!is.null(gene_gr)) {
-        p_interactive <- modify_markers(p_interactive)
-    }
-
-    final_title <- paste("CNV Genome Plot With Genes –", paste(geneNames, collapse = ", "))
-    if (nchar(gene_annotation_message) > 0) {
-        final_title <- paste0(final_title, "\nNote: ", gene_annotation_message)
-    }
-    p_interactive <- plotly::layout(p_interactive, title = list(text = final_title))
     list(plot = p_interactive, cnv_analysis = cnv_obj)
 }
 
+
 # Example usage:
-geneNames <- c("BRCA1", "FOXA1", "TP53")
-RGset <- minfi::read.metharray(my_idat, force = TRUE)
-
-if (RGset@annotation[["array"]] == "IlluminaHumanMethylationEPICv2") {
-    RGset@annotation[["annotation"]] <- "20a1.hg38"
-    RGset@annotation[["Genome"]] <- "hg38"
-} else{
-    RGset@annotation[["Genome"]] <- "hg19"
-}
-
-result <- custom_cnv_plot_with_gene_annotation(RGset, myGenes)
-result$plot  # Interactive plotly object
+result <- annotate_cnv_with_genes(my_idat, geneNames)
+result$plot
