55a302332a03cb647278802969f96fb115eccc36
jonserr <jnsrro@gmail.com>
2025-08-17 22:57:27 -0400

    Update PactMethMatch.R
    removed bioc_version

--- PACT_scripts/PactMethMatch.R
+++ PACT_scripts/PactMethMatch.R
@@ -35,16 +35,7 @@ main_pkgs <- c("data.table", "openxlsx", "jsonlite", "RCurl", "readxl",
 message("\n================ Parameters input ================\n")
 message("token: ", token, "\ninputSheet: ", inputSheet, "\n")
 
-snapshot_date <- "2025-05-01"
-bioc_version  <- "3.22"
-
-options(
-    download.file.method = "curl",
-    repos = c(
-        CRAN = sprintf("https://packagemanager.posit.co/cran/%s", snapshot_date),
-        BiocManager::repositories(version = bioc_version)
-    )
-)
+options(repos = c(CRAN = "https://cran.r-project.org"))
 
 if (!"devtools" %in% rownames(installed.packages())) {
     install.packages("devtools", ask = FALSE, dependencies = TRUE)
