7f2ece53869e568f34284b836da4cb04ca91eaf1
jonserr <jnsrro@gmail.com>
2025-08-17 22:56:17 -0400

    Update hs_metric_consensus.R
    removed snapshot date

--- PACT_scripts/hs_metric_consensus.R
+++ PACT_scripts/hs_metric_consensus.R
@@ -32,16 +32,7 @@ user_lib <- Sys.getenv("R_LIBS_USER", unset = "~/R/library")
 dir.create(user_lib, showWarnings = FALSE, recursive = TRUE)
 .libPaths(c(user_lib, .libPaths()))
 
-snapshot_date <- "2025-05-01"
-bioc_version  <- "3.22"
-
-options(
-    download.file.method = "curl",
-    repos = c(
-        CRAN = sprintf("https://packagemanager.posit.co/cran/%s", snapshot_date),
-        BiocManager::repositories(version = bioc_version)
-    )
-)
+options(repos = c(CRAN = "https://cran.r-project.org"))
 
 if (!"devtools" %in% rownames(installed.packages())) {
     install.packages("devtools", ask = FALSE, dependencies = TRUE)
