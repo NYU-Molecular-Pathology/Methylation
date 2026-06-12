d532443dc1fbd3b7ff2531739b6c4a80a4d3a43b
Jon <jnsrro@gmail.com>
2026-06-12 11:54:18 -0400

    Change condition for message output in check_csv_data
    Fixed object reference issue for worksheet

--- R/pipelineHelper.R
+++ R/pipelineHelper.R
@@ -876,10 +876,10 @@ check_csv_data <- function(sheetName = "samplesheet.csv") {
     if (sheetRunID != gb$runID) {
         message("Batch ID in sheet is: ", sheetRunID)
         message("Batch ID provided is: ", gb$runID)
-        if (!stringr::str_detect(pattern = "new-template", gb$runID)) {
-            message(worksheet$Project[1])
+        if (stringr::str_detect(pattern = "new-template", gb$runID)) {
+            message(csv_data$Project[1])
         } else {
-            stopifnot(gb$runID == worksheet$Project[1])
+            stopifnot(gb$runID == csv_data$Project[1])
         }
     }
 
